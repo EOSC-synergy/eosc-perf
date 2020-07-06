5d1a2bca989d58bc2b8d040ba9592c3ff6e3d384
Christophe <christophe.misc+git@protonmail.ch>
2020-07-06 06:34:29 +0200

    Improve string representations

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from app.model.data_types import Uploader, Site, Benchmark, Result, Tag
 db.create_all()
 
 uploader1 = Uploader(email='user1@example.com')
+uploader2 = Uploader(email='user@example.com')
 db.session.add(uploader1)
 
 site1 = Site(short_name='rpi', address='192.168.1.2', description="My cool raspberry pi")
@@ -14,12 +15,28 @@ db.session.add(site1)
 db.session.add(site2)
 
 benchmark1 = Benchmark(docker_name='user/bench:version', uploader=uploader1)
+benchmark2 = Benchmark(docker_name='user/otherbench:version', uploader=uploader1)
 db.session.add(benchmark1)
+db.session.add(benchmark2)
 
 tag1 = Tag(name='neato')
 tag2 = Tag(name='cpu')
+db.session.add(tag1)
+db.session.add(tag2)
 
-result1 = Result(json="bad", uploader=uploader1, site=site1, benchmark=benchmark1, tags=[tag1, tag2])
-result2 = Result(json="good", uploader=uploader1, site=site2, benchmark=benchmark1, tags=[tag2])
+uploaders = [uploader1, uploader2]
+sites = [site1, site2]
+benchmarks = [benchmark1, benchmark2]
+tags = [tag1, tag2]
+
+results = []
+# massively generate results for resultiterator tests
+for uploader in uploaders:
+    for site in sites:
+        for benchmark in benchmarks:
+            for tag in tags:
+                result = Result(json="{}", uploader=uploader, site=site, benchmark=benchmark, tags=[tag])
+                results.append(result)
+                db.session.add(result)
 
 db.session.commit()
