ebe1690dfb7a1cff531038b69e2b8c18762aae42
Christophe <christophe.misc+git@protonmail.ch>
2020-07-09 08:06:06 +0200

    Set up flask app for testing ajax

---
+++ app/model/sandbox.py
@@ -0,0 +1,34 @@
+from .data_types import *
+from .database import db
+
+uploaders = [Uploader(email='user1@example.com'), Uploader(email='user@example.com')]
+sites = [Site(short_name='rpi', address='192.168.1.2', description="My cool raspberry pi"), Site(short_name="terrapc", address='127.0.0.1', description="My strong desktop PC")]
+benchmarks = [Benchmark(docker_name='user/bench:version', uploader=uploaders[0]), Benchmark(docker_name='user/otherbench:version', uploader=uploaders[0])]
+tags = [Tag(name='neato'), Tag(name='cpu')]
+results = []
+for uploader in uploaders:
+   for site in sites:
+        for benchmark in benchmarks:
+             for tag in tags:
+                result = Result(json="{}", uploader=uploader, site=site, benchmark=benchmark, tags=[tag])
+                results.append(result)
+
+def add_dummy_objects(app):
+    app.app_context().push()
+    global uploaders
+    for uploader in uploaders:
+        db.session.add(uploader)
+
+    for site in sites:
+        db.session.add(site)
+    
+    for benchmark in benchmarks:
+        db.session.add(benchmark)
+
+    for tag in tags:
+        db.session.add(tag)
+
+    for result in results:
+        db.session.add(result)
+
+    db.session.commit()
