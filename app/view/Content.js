ac2a969ee6c7661f0d862fb4dae46104011cf1ae
Marc <uemzf@kit.edu>
2020-07-10 21:06:21 +0200

    Add Content.js abstract class

---
+++ app/view/Content.js
@@ -0,0 +1,7 @@
+/*The Content class acts as a templateforthe client-side Java Script classes
+*/
+function Content() {
+    //Update the appearance of the current page to match the state of the parameters.
+    //May get changed if multiple subclasses require the same update precedure.
+    this.update() = new function() { };
+}
