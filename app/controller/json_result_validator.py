cb984439505cdacbecb95f4303a514e1f12ab977
mlbehr <behr.moritz@googlemail.com>
2020-07-11 17:42:25 +0200

    Fixed check_subkeys parameter in same_keys function

--- app/controller/json_result_validator.py
+++ app/controller/json_result_validator.py
@@ -34,9 +34,10 @@ def _same_keys(json_one, json_two, check_subkeys=True):
     # Check if both dictionaries have the same keys
     if not set(json_one.keys()) == set(json_two.keys()):
         return False
-    # Check if both dictionaries have the same subkeys
-    keys_with_dict_values = [key for key in json_one.keys() if type(json_one[key]) == dict]
-    for key in keys_with_dict_values:
-        if not _same_keys(json_one[key], json_two[key]):
-            return False
+    if check_subkeys:
+        # Check if both dictionaries have the same subkeys
+        keys_with_dict_values = [key for key in json_one.keys() if type(json_one[key]) == dict]
+        for key in keys_with_dict_values:
+            if not _same_keys(json_one[key], json_two[key]):
+                return False
     return True
