ee8971268f2c4a19a1caa4eee6198980d78ea3ec
mlbehr <behr.moritz@googlemail.com>
2020-07-11 17:16:59 +0200

    JSON validator now checks subkeys

--- app/controller/json_result_validator.py
+++ app/controller/json_result_validator.py
@@ -23,12 +23,20 @@ class JSONResultValidator:
         return self._has_mandatory_fields(result_json)
 
     # Check wether the given json has the same keys as the template
-    # TODO: currently NOT checking subkeys! May have to add that later
     def _has_mandatory_fields(self, result_json):
         return _same_keys(result_json, self.template_json)
 
 
-def _same_keys(json_one, json_two):
+def _same_keys(json_one, json_two, check_subkeys=True):
+    # Check if both parameters are dictionaries
     if not type(json_one) == type(json_two) == dict:
         return False
-    return set(json_one.keys()) == set(json_two.keys())
+    # Check if both dictionaries have the same keys
+    if not set(json_one.keys()) == set(json_two.keys()):
+        return False
+    # Check if both dictionaries have the same subkeys
+    keys_with_dict_values = [key for key in json_one.keys() if type(json_one[key]) == dict]
+    for key in keys_with_dict_values:
+        if not _same_keys(json_one[key], json_two[key]):
+            return False
+    return True
