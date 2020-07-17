cd8e655ec81670e11d334fabdd660ebfb0c242d4
Jonas Blahut <j@die-blahuts.de>
2020-07-17 21:15:08 +0000

    Added implementations for all basic filters

---
+++ app/model/filters.py
@@ -0,0 +1,58 @@
+""""This module contains all the filters.
+Provided are:
+  - Filter
+  - BenchmarkFilter
+  - UploaderFilter
+  - TagFilter
+  - JsonValueFilter"""
+from app.model.data_types import Result, Tag, Benchmark, Uploader, Site, Report, ResultIterator
+from abc import abstractmethod
+from functools import reduce
+import json
+
+class Filter:
+    """Abstract filter that all concrete Filter implementations inherit from."""
+    @abstractmethod
+    def filter(self, result: Result) -> bool:
+        """Returns whether an element matches the filter or not."""
+
+class BenchmarkFilter(Filter):
+    """Filter implementation that matches on Benchmark docker_name."""
+    def __init__(self, docker_name: str):
+        self.docker_name = docker_name
+    
+    def filter(self, result: Result) -> bool:
+        """Returns whether the result belongs to the benchmark the filter was primed with."""
+        return result.get_benchmark().get_docker_name() == self.docker_name
+        
+class UploaderFilter(Filter):
+    """Filter implementation that matches on uploader email."""
+    def __init__(self, email: str):
+        self.email = email
+    
+    def filter(self, result: Result) -> bool:
+        """Returns whether the result was uploaded by the user the filter was primed with."""
+        return result.get_uploader().get_email() == self.email
+        
+class TagFilter(Filter):
+    """Filter implementation that matches on tags."""
+    def __init__(self, tag: str):
+        self.tag = tag
+    
+    def filter(self, result: Result) -> bool:
+        """Returns whether the result has the tag the filter was primed with."""
+        return self.tag in [t.get_name() for t in result.get_tags()]
+        
+class JsonValueFilter(Filter):
+    """Filter implementation that matches on a value in the JSON document."""
+    def _deep_get(self, dictionary, keys, default=None):
+        """Helper to get element based on JSON template"""
+        return reduce(lambda d, key: d.get(key, default) if isinstance(d, dict) else default, keys.split("."), dictionary)
+
+    def __init__(self, template: str, value: str):
+        self.template = template
+        self.value = value
+    
+    def filter(self, result: Result) -> bool:
+        """Returns whether the result has the property the filter was primed with."""
+        return self._deep_get(json.loads(result.get_json()), self.template) == self.value
