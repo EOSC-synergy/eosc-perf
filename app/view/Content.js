db17dea2d9ab8b5570998a14e858372866f0ff13
Christophe <christophe.misc+git@protonmail.ch>
2020-07-20 13:53:20 +0200

    Make JS content code into a class

--- app/view/Content.js
+++
@@ -1,7 +0,0 @@
-/*The Content class acts as a template for the client-side JavaScript classes
-*/
-function Content() {
-    //Update the appearance of the current page to match the state of the parameters.
-    //May get changed if multiple subclasses require the same update precedure.
-    this.update() = new function() { };
-}
