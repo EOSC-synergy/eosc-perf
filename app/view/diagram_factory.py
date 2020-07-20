d838c63f3b9e24cd43df4cbcc8fd6de87eff6a93
Christophe <christophe.misc+git@protonmail.ch>
2020-07-20 23:35:33 +0200

    Implement diagram page and factory

---
+++ app/view/diagram_factory.py
@@ -0,0 +1,45 @@
+"""This module contains the factory to generate result-comparison diagram
+pages.
+Provided is:
+ - DiagramFactory
+"""
+
+import json
+
+from flask import request, Response
+from flask.blueprints import Blueprint
+
+from .page_factory import PageFactory
+from .type_aliases import HTML, JSON
+
+
+class DiagramFactory(PageFactory):
+    """A factory to build diagram pages."""
+
+    def __init__(self):
+        super().__init__()
+
+    def _generate_content(self, args: JSON) -> HTML:
+        pass
+
+    # def set_info(self, info: str):
+    #    pass
+
+
+diagram_blueprint = Blueprint('diagram-factory', __name__)
+
+
+@diagram_blueprint.route('/make_diagram')
+def query_results():
+    """HTTP endpoint for diagram generation page"""
+    uuids = request.args.getlist('result_uuids')
+    if uuids is None:
+        # TODO: error page
+        return None
+    else:
+        factory = DiagramFactory()
+        factory.set_info("benchmark x page y")
+        args = {'uuids': uuids}
+        with open('templates/diagram.html') as file:
+            page = factory.generate_page(json.dumps(args), file.read())
+        return Response(page, mimetype='text/html')
