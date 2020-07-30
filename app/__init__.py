06ba455ec1284b8ff40bc3929ee7a85ba46e795d
mlbehr <behr.moritz@googlemail.com>
2020-07-30 23:12:39 +0200

    Added basic oidc integration for authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 # package root
 from flask import Flask, request
+from .controller.authenticator import authenticator
 from .model.database import db, DATABASE_PATH, configure_database
 from .model.facade import facade
 from .model.sandbox import add_dummies_if_not_exist
@@ -10,10 +11,12 @@ import code
 
 
 def create_app(debug: bool):
-    app = Flask(__name__)
+    app = Flask(__name__)    
     if debug:
         app.config['DEBUG'] = True
     configure_database(app)
+
+    authenticator(app)
 
     if debug:
         add_dummies_if_not_exist(app)
--- app/controller/authenticator.py
+++ app/controller/authenticator.py
@@ -1,37 +1,54 @@
 '''This module acts as a facade between the IOController
 and the EGI Check-In authentication system'''
 
+from flask import Flask, url_for, redirect, session
+from authlib.integrations.flask_client import OAuth
+
 DEFAULT_AUTH_URL = ""
+CONF_URL = "https://aai-dev.egi.eu/oidc/.well-known/openid-configuration"
 
-class Authenticator:
+def authenticator(app):
+    app.secret_key = '!secret'
+    app.config["EOSC-PERF_CLIENT_ID"] = "eosc-perf"
+    app.config["EOSC-PERF_CLIENT_SECRET"] = "UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw"
 
-    def __init__(self, auth_url=DEFAULT_AUTH_URL):
-        self.auth_url = auth_url
-        self.token = None
+    oauth = OAuth(app)
+    oauth.register(
+        name="eosc-perf",
+        server_metadata_url=CONF_URL,
+        client_kwargs={
+            'scope': 'openid email profile'
+        },
+        secret="UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw"
+    )
 
-    def authenticate_user(self):
-        #TODO: implement
-        return False
+    @app.route('/login')
+    def authenticate_user():
+        redirect_uri = "https://localhost" + url_for('is_authenticated', _external=False)
+        return oauth._clients["eosc-perf"].authorize_redirect(redirect_uri)
 
-    def is_authenticated(self):
-        #TODO: implement
-        return False
+    @app.route('/oidc-redirect')
+    def is_authenticated():
+        token = oauth.eosc_perf.authorize_access_token()
+        user = oauth.eosc_perf.parse_id_token(token)
+        session['user'] = user
+        return redirect('/')
 
     def sign_out(self):
-        #TODO: implement
-        return False
+        session.pop('user', None)
+        return redirect('/')
 
     def is_admin(self):
-        #TODO: implement
+        # TODO: implement
         return False
 
     def get_auth_url(self):
         return self.auth_url
 
     def _set_token(self):
-        #TODO: implement
+        # TODO: implement
         self.token = None
 
     def _val_token(self):
-        #TODO: implement
+        # TODO: implement
         return False
