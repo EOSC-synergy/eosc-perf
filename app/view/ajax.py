2c0c430e6e4bcf9f5e0b78bed15a9f9feceb9721
Jonas Blahut <j@die-blahuts.de>
2020-08-06 01:59:34 +0000

    Implement BenchmarkSearchAJAX

--- app/view/ajax.py
+++ app/view/ajax.py
@@ -60,7 +60,25 @@ class ResultSearchAJAX(SearchAJAXHandler):
 class BenchmarkSearchAJAX(SearchAJAXHandler):
     """AJAX handler for benchmark searches with keywords."""
 
+    def __init__(self, facade_: DatabaseFacade):
+        """Set up a new BenchmarkSearchAJAX using the DatabaseFacade."""
+        self._facade = facade_
+        
+    def find_results(self, query: JSON) -> JSON:
+        """Fetch benchmarks corresponding to given query."""
+        results_dict = {"results": []}
+        keywords = json.loads(query)['keywords']
+        benchmarks = self._facade.query_benchmarks(keywords)
+        
+        for benchmark in benchmarks:
+            result_dict = {}
+            # decode and add to structure to avoid dealing with storing json within jsonj
+            result_dict["docker_name"] = benchmark.get_docker_name()
+            result_dict["uploader"] = benchmark.get_uploader().get_email()
+            results_dict["results"].append(result_dict)
 
+        return json.dumps(results_dict)
+    
 ajax_blueprint = Blueprint('ajax', __name__)
 
 
@@ -71,4 +89,13 @@ def query_results():
     if query_json is None:
         query_json = "{}"
     handler = ResultSearchAJAX(facade)
+    return Response(handler.fetch_data(query_json), mimetype='application/json')
+    
+@ajax_blueprint.route('/query_benchmarks')
+def query_benchmarks():
+    """HTTP endpoint for benchmark AJAX queries."""
+    query_json = request.args.get('query_json')
+    if query_json is None:
+        query_json = "{}"
+    handler = BenchmarkSearchAJAX(facade)
     return Response(handler.fetch_data(query_json), mimetype='application/json')
