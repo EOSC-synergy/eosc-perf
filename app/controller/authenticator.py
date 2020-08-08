24d8d8ee5731ffcd7cbed1c68bf1d56a73c8b705
mlbehr <behr.moritz@googlemail.com>
2020-08-08 14:14:54 +0200

    Users keep authenticated until token expires

--- app/controller/authenticator.py
+++ app/controller/authenticator.py
@@ -1,13 +1,16 @@
 '''This module acts as a facade between the IOController
 and the EGI Check-In authentication system'''
 
-from flask import Flask, url_for, redirect, session
+from time import time
+
+from flask import url_for, redirect, session
 from authlib.integrations.flask_client import OAuth
 
-DEFAULT_AUTH_URL = ""
 CONF_URL = "https://aai-dev.egi.eu/oidc/.well-known/openid-configuration"
 
 def authenticator(app):
+    """Sets up OIDC authentication functionality for the web app"""
+
     app.secret_key = '!secret'
     app.config["EOSC-PERF_CLIENT_ID"] = "eosc-perf"
     app.config["EOSC-PERF_CLIENT_SECRET"] = "UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw"
@@ -24,31 +27,47 @@ def authenticator(app):
 
     @app.route('/login')
     def authenticate_user():
-        redirect_uri = "https://localhost" + url_for('is_authenticated', _external=False)
+        """Redirects user to EGI Check-In for authentication"""
+        redirect_uri = "https://localhost" + url_for('authentication_redirect', _external=False)
         return oauth._clients["eosc-perf"].authorize_redirect(redirect_uri)
 
     @app.route('/oidc-redirect')
-    def is_authenticated():
-        token = oauth._clients["eosc-perf"].authorize_access_token()
-        user = oauth._clients["eosc-perf"].parse_id_token(token)
-        session['user'] = user
-        return redirect('/')
+    def authentication_redirect():
+        """Validates user authentication after login through EGI Check-In"""
+        if is_authenticated():
+            return "Logged in successfully"
+        else:
+            return "Login failed"
 
-    def sign_out(self):
+    def is_authenticated():
+        """Checks if the current user is authenticated. Will return true
+           if the user just logged in through EGI Check-In or if the user
+           still has a token that is not expired."""
+        if not __token_expired():
+            return True
+        try:
+            token = oauth._clients["eosc-perf"].authorize_access_token()
+            user = oauth._clients["eosc-perf"].parse_id_token(token)
+            session['user'] = user
+        except KeyError:
+            return False
+        return True
+
+    def sign_out():
+        """Signs out the current user"""
         session.pop('user', None)
         return redirect('/')
 
-    def is_admin(self):
-        # TODO: implement
-        return False
+    def __token_expired():
+        """Checks if the current user has a non-expired authentication token"""
+        try:
+            user = session['user']
+            print(user)
+            return user['exp'] < time()
+        except KeyError:
+            return True
 
-    def get_auth_url(self):
-        return self.auth_url
-
-    def _set_token(self):
-        # TODO: implement
-        self.token = None
-
-    def _val_token(self):
+    def is_admin():
+        """Checks wether the current user has admin rights"""
         # TODO: implement
         return False
