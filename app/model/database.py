ae4dbbd173ad1aafbe24513966950af22b9fe61c
Christophe <christophe.misc+git@protonmail.ch>
2020-08-08 02:51:41 +0200

    Fix database not clearing in debug mode

--- app/model/database.py
+++ app/model/database.py
@@ -2,25 +2,27 @@
 import os.path
 from flask_sqlalchemy import SQLAlchemy
 
-DATABASE_PATH: str = 'test.db'
-
 db = SQLAlchemy()
 
 def configure_database(app, config):
     """Set up the database with the given flask app."""
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DATABASE_PATH
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # delete database on debug launch
-    if config['debug']:
-        if os.path.exists(DATABASE_PATH):
-            os.remove(DATABASE_PATH)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + config['database-path']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # setup sqlalchemy for flask
     db.init_app(app)
 
+    # delete database on debug launch
+    if config['debug']:
+        if os.path.exists(config['database-path']):
+            os.remove(config['database-path'])
+        # drop everything
+        with app.app_context():
+            db.drop_all()
+
     # create database if it does not exist
-    if not os.path.exists(DATABASE_PATH):
+    if not os.path.exists(config['database-path']):
         with app.app_context():
             db.create_all()
         
