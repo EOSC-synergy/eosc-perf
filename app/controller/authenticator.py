c86c7f2f016d0999e229ecb871defac35216ba02
mlbehr <behr.moritz@googlemail.com>
2020-08-08 16:22:49 +0200

    Made authenticator a class

--- app/controller/authenticator.py
+++ app/controller/authenticator.py
@@ -1,5 +1,7 @@
 '''This module acts as a facade between the IOController
-and the EGI Check-In authentication system'''
+and the EGI Check-In authentication system.
+Provided is:
+ - Authenticator'''
 
 from time import time
 
@@ -8,58 +10,62 @@ from authlib.integrations.flask_client import OAuth
 
 CONF_URL = "https://aai-dev.egi.eu/oidc/.well-known/openid-configuration"
 
-def authenticator(app):
-    """Sets up OIDC authentication functionality for the web app"""
+class Authenticator:
+    """A fascade between IOController and the EGI Check-In authentication 
+       system. It integrates Open ID Connect into the web app."""
 
-    app.secret_key = '!secret'
-    app.config["EOSC-PERF_CLIENT_ID"] = "eosc-perf"
-    app.config["EOSC-PERF_CLIENT_SECRET"] = "UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw"
+    def __init__(self):
+        self.oauth = None
 
-    oauth = OAuth(app)
-    oauth.register(
-        name="eosc-perf",
-        server_metadata_url=CONF_URL,
-        client_kwargs={
-            'scope': 'openid email profile'
-        },
-        secret="UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw"
-    )
+    def configure_authenticator(self, flask_app):
+        """Sets up OIDC authentication functionality for the web app"""
 
-    @app.route('/login')
-    def authenticate_user():
+        flask_app.secret_key = '!secret'
+        flask_app.config["EOSC-PERF_CLIENT_ID"] = "eosc-perf"
+        flask_app.config["EOSC-PERF_CLIENT_SECRET"] = "UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw"
+
+        self.oauth = OAuth(flask_app)
+        self.oauth.register(
+            name="eosc-perf",
+            server_metadata_url=CONF_URL,
+            client_kwargs={
+                'scope': 'openid email profile'
+            },
+            secret="UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw"
+        )
+
+    def authenticate_user(self, redirect_uri):
         """Redirects user to EGI Check-In for authentication"""
-        redirect_uri = "https://localhost" + url_for('authentication_redirect', _external=False)
-        return oauth._clients["eosc-perf"].authorize_redirect(redirect_uri)
+        return self.oauth._clients["eosc-perf"].authorize_redirect(redirect_uri)
 
-    @app.route('/oidc-redirect')
-    def authentication_redirect():
+    def authentication_redirect(self):
         """Validates user authentication after login through EGI Check-In"""
-        if is_authenticated():
+        if self.is_authenticated():
             return "Logged in successfully"
         else:
             return "Login failed"
 
-    def is_authenticated():
+    def is_authenticated(self):
         """Checks if the current user is authenticated. Will return true
            if the user just logged in through EGI Check-In or if the user
            still has a token that is not expired."""
-        if not __token_expired():
+        if not self.__token_expired():
             return True
         try:
-            token = oauth._clients["eosc-perf"].authorize_access_token()
-            user = oauth._clients["eosc-perf"].parse_id_token(token)
+            token = self.oauth._clients["eosc-perf"].authorize_access_token()
+            user = self.oauth._clients["eosc-perf"].parse_id_token(token)
             session['user'] = user
         except KeyError:
             return False
         return True
 
-    def sign_out():
+    def sign_out(self):
         """Signs out the current user"""
         session.pop('user', None)
         return redirect('/')
 
-    def __token_expired():
-        """Checks if the current user has a non-expired authentication token"""
+    def __token_expired(self):
+        """Checks if the current user has a valid authentication token"""
         try:
             user = session['user']
             print(user)
@@ -67,7 +73,23 @@ def authenticator(app):
         except KeyError:
             return True
 
-    def is_admin():
+    def is_admin(self):
         """Checks wether the current user has admin rights"""
         # TODO: implement
         return False
+
+# single global instance
+authenticator = Authenticator()
+
+def configure_authenticator(app):
+    authenticator.configure_authenticator(app)
+
+    @app.route("/login")
+    def authenticate_user():
+        redirect_uri = "https://localhost" \
+                       + url_for('authentication_redirect', _external=False)
+        return authenticator.authenticate_user(redirect_uri)
+
+    @app.route("/oidc-redirect")
+    def authentication_redirect():
+        return authenticator.authentication_redirect()
