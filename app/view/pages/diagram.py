4c16de1ffad54023898457b62605bc1904131138
Christophe <christophe.misc+git@protonmail.ch>
2020-08-08 03:53:58 +0200

    Allow passing uuids to diagram factory in url
    I waited for the person responsible for the search page on how they
    would like to submit data to the diagram page, but they have neither
    responded nor completed their page, so I'm imposing a format.

--- app/view/pages/diagram.py
+++ app/view/pages/diagram.py
@@ -8,16 +8,56 @@ import json
 
 from flask import request, Response, redirect
 from flask.blueprints import Blueprint
+from werkzeug.urls import url_encode
 
 from ..page_factory import PageFactory
 from ..type_aliases import HTML, JSON
+
+from ...model.facade import facade
 
 
 class DiagramFactory(PageFactory):
     """A factory to build diagram pages."""
 
-    def _generate_content(self, args: JSON) -> HTML:
-        pass
+    def generate_script_content(self, uuids) -> HTML:
+        """Generate script data for the diagram."""
+        results = []
+        for uuid in uuids:
+            result_json = facade.get_result(uuid)
+            # pulling at straws here, but hoping this is useful data
+            result = json.loads(result_json.get_json())
+            core_count = result['user_args']['num_gpus']
+            score = result['evaluation']['result']['average_examples_per_sec']
+            results.append({'core_count': core_count, 'score': score})
+        
+        # sort them by core_count
+        results.sort(key=lambda result: result['core_count'])
+        script_content = ', '.join([json.dumps(result) for result in results])
+        print(script_content)
+        return script_content
+
+    def generate_page_content(self, uuids) -> HTML:
+        """Generate page body code.
+
+        This contains a list of compared results."""
+        list_start = 'Comparing elements:'
+        list_elements = []
+        results = [facade.get_result(uuid) for uuid in uuids]
+        for result in results:
+            core_count = json.loads(result.get_json())['user_args']['num_gpus']
+            list_elements.append('[{} - {} - {}]'.format(
+                result.get_site().get_name(),
+                result.get_benchmark().get_docker_name(),
+                core_count))
+        return list_start + ', '.join(list_elements)
+    
+    def check_if_results_exist(self, uuids) -> bool:
+        for uuid in uuids:
+            try:
+                facade.get_result(uuid)
+            except facade.NotFoundError:
+                return False
+        return True
 
 
 diagram_blueprint = Blueprint('diagram-factory', __name__)
@@ -27,12 +67,23 @@ diagram_blueprint = Blueprint('diagram-factory', __name__)
 def query_results():
     """HTTP endpoint for diagram generation page"""
     uuids = request.args.getlist('result_uuids')
-    if uuids is None:
-        return redirect("/error?text=Diagram%20page%20called%20with%20invalid%20arguments", code=302)
-
+    if len(uuids) == 0:
+        return redirect('/error?' + url_encode({
+            'text': 'Diagram page called with invalid arguments'}), code=302)
+    
     factory = DiagramFactory()
-    factory.set_info("benchmark x page y")
+
+    if not factory.check_if_results_exist(uuids):
+        return redirect('/error?' + url_encode({
+            'text': 'At least one result given to diagram does not exist'}), code=302)
+    
+    
     args = {'uuids': uuids}
+
     with open('templates/diagram.html') as file:
-        page = factory.generate_page(json.dumps(args), file.read())
+        page = factory.generate_page(
+            args=json.dumps(args),
+            template=file.read(),
+            page_content=factory.generate_page_content(uuids),
+            script_content=factory.generate_script_content(uuids))
     return Response(page, mimetype='text/html')
