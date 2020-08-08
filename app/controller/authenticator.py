b447c284678a1a2860c96faa32968d279cb33e59
mlbehr <behr.moritz@googlemail.com>
2020-08-08 23:36:28 +0200

    Changes to please pylint

--- app/controller/authenticator.py
+++ app/controller/authenticator.py
@@ -5,24 +5,25 @@ Provided is:
 
 from time import time
 
-from flask import url_for, redirect, session
+from flask import redirect, session
 from authlib.integrations.flask_client import OAuth
 
 CONF_URL = 'https://aai-dev.egi.eu/oidc/.well-known/openid-configuration'
 
 class Authenticator:
-    """A fascade between IOController and the EGI Check-In authentication 
+    """A fascade between IOController and the EGI Check-In authentication
        system. It integrates Open ID Connect into the web app."""
 
     def __init__(self):
         self.oauth = None
 
-    def configure_authenticator(self, flask_app):
+    def configure_authenticator(self, flask_app, config):
         """Sets up OIDC authentication functionality for the web app"""
+        client_secret = config['oidc_client_secret']
 
         flask_app.secret_key = '!secret'
-        flask_app.config["EOSC-PERF_CLIENT_ID"] = "eosc-perf"
-        flask_app.config["EOSC-PERF_CLIENT_SECRET"] = "UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw"
+        flask_app.config["EOSC-PERF_CLIENT_ID"] = 'eosc-perf'
+        flask_app.config["EOSC-PERF_CLIENT_SECRET"] = client_secret
 
         self.oauth = OAuth(flask_app)
         self.oauth.register(
@@ -32,7 +33,7 @@ class Authenticator:
             client_kwargs={
                 'scope': 'openid email profile'
             },
-            secret='UV0xI-uY9Hd-Z12Nc9gGNvajwpHdXtiYPAAbqASfsWqR9esSGj3jRZkLZO0-ndfoHAgYSEUD-OM5wivNwCIXyw'
+            secret=client_secret
         )
 
     def authenticate_user(self):
@@ -44,8 +45,7 @@ class Authenticator:
         """Validates user authentication after login through EGI Check-In"""
         if self.is_authenticated():
             return 'Logged in successfully'
-        else:
-            return 'Login failed'
+        return 'Login failed'
 
     def is_authenticated(self):
         """Checks if the current user is authenticated. Will return true
@@ -59,39 +59,42 @@ class Authenticator:
             userinfo = self.oauth._clients['eosc-perf'].userinfo()
             session['user'] = user
             session['mail'] = userinfo['email']
-            print(userinfo)
         except KeyError:
             return False
         return True
 
-    def sign_out(self):
-        """Signs out the current user"""
-        session.pop('user', None)
-        return redirect('/')
-
     def get_email(self):
+        """Returns the email address of the current user.
+           If no user is logged in, an empty string is returned"""
         if not self.__token_expired():
             return session['mail']
-
-    def __token_expired(self):
-        """Checks if the current user has a valid authentication token"""
-        try:
-            user = session['user']
-            print(user)
-            return user['exp'] < time()
-        except KeyError:
-            return True
+        return ""
 
     def is_admin(self):
         """Checks wether the current user has admin rights"""
         # TODO: implement
         return False
 
+
+def __token_expired():
+    """Checks if the current user has a valid authentication token"""
+    try:
+        user = session['user']
+    except KeyError:
+        return True
+    return user['exp'] < time()
+
+
+def sign_out():
+    """Signs out the current user"""
+    session.pop('user', None)
+    return redirect('/')
+
 # single global instance
 authenticator = Authenticator()
 
-def configure_authenticator(app):
-    authenticator.configure_authenticator(app)
+def configure_authenticator(app, config):
+    authenticator.configure_authenticator(app, config)
 
     @app.route('/login')
     def authenticate_user():
