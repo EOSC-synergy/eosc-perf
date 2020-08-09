91f2d3fd3b7f86c6f75185d6d6e7787d8f5a9683
Christophe <christophe.misc+git@protonmail.ch>
2020-08-09 20:25:00 +0200

    Simplify model sandbox code

--- model_sandbox.py
+++ model_sandbox.py
@@ -2,14 +2,11 @@ from typing import List
 import code
 from app import app
 from app.model.database import db
-from app.model.sandbox import add_dummies_if_not_exist, results, benchmarks, sites, tags, uploaders
-from app.model.data_types import Uploader, Tag, Result, Benchmark, Site, ResultIterator, Report, \
-    SiteReport, BenchmarkReport, ResultReport
+from app.model.sandbox import *
+from app.model.data_types import *
 from app.model.facade import facade
 
 # utility helpers for functions not part of design
-
-
 def get_uploaders() -> List[Uploader]:
     """Get all uploaders."""
     # prepare query
@@ -20,8 +17,5 @@ def get_benchmarks() -> List[Benchmark]:
     """Get all benchmarks."""
     # prepare query
     return db.session.query(Benchmark).all()
-
-
-#add_dummies_if_not_exist(app)
 
 code.interact(local=locals())
