c61eaaf428a4de210f68ec12d054c4dbd62b09b8
mlbehr <behr.moritz@googlemail.com>
2020-08-10 18:57:50 +0200

    Added benchmark search page

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ from .view.pages.information_page import info_blueprint
 from .view.pages.error_page import error_blueprint
 from .view.pages.report_result import result_report_blueprint
 from .view.pages.benchmark_review import benchmark_review_blueprint
+from .view.pages.benchmark_search import benchmark_search_blueprint
 from .view.pages.site_review import site_review_blueprint
 from .view.pages.view_report import view_report_blueprint
 from .view.pages.add_benchmark import add_benchmark_blueprint
@@ -54,6 +55,7 @@ def create_app(config):
     flask_app.register_blueprint(site_review_blueprint)
     flask_app.register_blueprint(view_report_blueprint)
     flask_app.register_blueprint(add_benchmark_blueprint)
+    flask_app.register_blueprint(benchmark_search_blueprint)
     flask_app.register_blueprint(authenticator_blueprint)
 
     return flask_app
--- app/view/pages/benchmark_search.py
+++ app/view/pages/benchmark_search.py
@@ -4,10 +4,11 @@ Provided is:
  - BenchmarkSearchFactory
 """
 
+from flask import Response, redirect
+from flask.blueprints import Blueprint
+
 from ..page_factory import PageFactory
 from ..type_aliases import HTML, JSON
-
-from flask.blueprints import Blueprint
 
 
 class BenchmarkSearchFactory(PageFactory):
@@ -20,4 +21,20 @@ class BenchmarkSearchFactory(PageFactory):
         # Stub
         return "Search"
 
-        
+benchmark_search_blueprint = Blueprint('benchmark-search', __name__)
+
+@benchmark_search_blueprint.route('/benchmark_search', methods=['GET'])
+def search_benchmark():
+    """HTTP endpoint for the benchmark search page"""
+    factory = BenchmarkSearchFactory()
+
+    with open('templates/benchmark_search.html') as file:
+        page = factory.generate_page(
+            args='{}',
+            template=file.read())
+    return Response(page, mimetype='text/html')
+
+@benchmark_search_blueprint.route('/benchmark_search_submit', methods=['POST'])
+def search_benchmark_submit():
+    """HTTP endpoint to take in benchmark searches"""
+    return redirect('/')
