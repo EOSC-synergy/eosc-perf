f5c0bd690abccd28bc89851ec8e90d28aa852784
mlbehr <behr.moritz@googlemail.com>
2020-08-11 01:12:04 +0200

    Added benchmark search functionality

--- static/scripts/view/benchmark_search.js
+++ static/scripts/view/benchmark_search.js
@@ -1,7 +1,25 @@
 $(function () {
     form = $('#form');
     form.submit(function (e) {
-        alert('Search button clicked.');
+        query = form.find('input[name="query"]').val().trim();
+        if (query === "") {
+            return false;
+        }
+        table = document.getElementById("result_table");
+        keywords = query.split(" ");
+        $.ajax('/query_benchmarks?query_json='
+            + encodeURI(JSON.stringify(
+                {'keywords':keywords})))
+            .done(function(data) {
+                results = data.results;
+                for (index = 0; index < results.length; ++index) {
+                    var result_row = table.insertRow(-1)
+                    var docker_name_cell = result_row.insertCell(0)
+                    var docker_name = results[index].docker_name
+                    docker_name_cell.innerHTML = docker_name
+                }
+                console.log(data);
+            })
         return false;
     });
 });
