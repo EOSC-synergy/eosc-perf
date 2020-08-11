3709a6a284be360fdb165db84361b1f1af78fba2
Marc <falkenbergjmarc@gmail.com>
2020-08-11 02:53:26 +0200

    Add and remove filter

--- static/scripts/view/search_results.js
+++ static/scripts/view/search_results.js
@@ -4,111 +4,120 @@
  */
 
 class ResultSearch extends Content {
-    constructor() {
-        this.query = ""
-        this.results = []
-        this.current_page = 1
-        this.selected_results = []
-        this.admin = false
-        this.filters = []
-        this.results_per_page = 10
-        this.ordered_by = null
-        this.columns = ["Benchmark", "Uploader", "Location", "Tags", "Json", "Report"]
-        this.values = ["benchmark", "uploader", "site", "tag", "json"]
-    }
-    update() {
+
+    query = ""
+    results = []
+    current_page = 1
+    selected_results = []
+    admin = false
+    filters = []
+    results_per_page = 10
+    ordered_by = null
+    columns = ["Selected", "Benchmark", "Uploader", "Location", "Tags", "Json", "Report"]
+    values = ["benchmark", "uploader", "site", "tag", "json"]
+
+    static update() {
         // Update table. 
         const creat_cell = (value, type) => "<" + type + ">" + value + "</" + type + ">\n"
         const create_row = (row, type) => "<tr>\n" +
             row.map(x => create_cell(x, type))
                 .reduce((x, y) => x + y) + "</tr>\n";
         // Create table head.
-        var table_str = create_row(this.columns, "th")
+        var table_str = create_row(columns, "th")
         // Create table body.
         // Calculate range.
-        var results_amount = this.results.length() - 1
-        var start = Math.min((this.current_page - 1) * this.results_per_page, results_amount)
-        var end = Math.min(start + this.results_per_page, results_amount)
+        var results_amount = results.length() - 1
+        var start = Math.min((current_page - 1) * results_per_page, results_amount)
+        var end = Math.min(start + results_per_page, results_amount)
         for (var i = start; i < end; i++) {
             table_str += "<tr>\n"
+            // Add select field.
+            if (results["selected"]) {
+                var box = "<input type=\"checkbox\" id = \"s" + i + "\">"
+                table_str += creat_cell(box, "td")
+            } else {
+
+            }
             // Add the values.
-            for (var value = 0; value < this.values.length; value++) {
-                table_str += create_cell(this.results[i][this.values[j]],"td")
+            for (var value = 0; value < values.length; value++) {
+                table_str += create_cell(results[i][values[j]], "td")
             }
             table_str += "</tr>\n"
         }
         // Set table in html.
-        $('#result_page').html(table_str)
+        $("#result_page").html(table_str);
 
     }
-    set_page(page) {
-        this.current_page = page
-        this.update()
+    static set_page(page) {
+        current_page = page
+        update()
     }
 
-    search(new_query) {
+    static search(new_query) {
         // Generate query.
         // Request the result query.
-        $.getJSON('/query_results', { 'query_json': JSON.stringify(this.query) },
-            function (data) { this.results = JSON.parse(data.result) })
+        $.getJSON('/query_results', { 'query_json': JSON.stringify(query) },
+            function (data) { results = JSON.parse(data.result) })
+        // Add select field.
+        results = results.map(result => result["selected"] = false)
     }
 
-    add_filter(filter) {
-        this.filters.push(filter)
+    static add_filter(filter) {
+        filters.push(filter)
     }
 
-    remove_filter(filter) {
-        this.filters = this.filters.filter(function (item) { item !== filter })
+    static remove_filter(filter) {
+        filters = filters.filter(function (item) { item !== filter })
     }
 
-    sort_by(colum) {
+    static sort_by(colum) {
 
-        this.results.sort(
+        results.sort(
             //todo
         )
         // reverse order if double clicked.
-        if (this.ordered_by == colum) {
-            this.results = this.results.reverse()
+        if (ordered_by == colum) {
+            results = results.reverse()
         }
-        this.ordered_by = colum
-        this.update()
+        ordered_by = colum
+        update()
     }
 
-    set_results_per_page(amount) {
-        this.results_per_page = amount
-        this.update()
+    static set_results_per_page(amount) {
+        results_per_page = amount
+        update()
     }
 
-    select_result(result) {
-        for (var i; i < this.results; i++) {
-            if (this.results[i] == result) {
-                this.selected_results.push(this.results[i])
+    static select_result(result) {
+        for (var i; i < results; i++) {
+            if (results[i] == result) {
+                selected_results.push(results[i])
                 break
             }
         }
     }
 
-    unselect_result(result) {
-        this.select_result = this.select_result.filter(function (item) { item !== filter })
+    static unselect_result(result) {
+        select_result = select_result.filter(function (item) { item !== filter })
     }
 
-    make_diagram() {
+    static make_diagram() {
         // Store data in href
     }
 
-    delete_result(result) {
+    static delete_result(result) {
 
     }
 
-    report_result(result) {
+    static report_result(result) {
 
     }
 
-    result_information(result) {
+    static result_information(result) {
 
     }
 
-    hide_information(result) {
+    static hide_information(result) {
 
     }
 }
