398cfa5b4bcec9a10e5a58bbb9b0277c88833304
mlbehr <behr.moritz@googlemail.com>
2020-08-12 15:13:50 +0200

    Finished benchmark search

--- static/scripts/view/benchmark_search.js
+++ static/scripts/view/benchmark_search.js
@@ -1,25 +1,80 @@
+var results_per_page = 25;
+var results = [];
+var result_amount = 0;
+var page = 0;
+
 $(function () {
     form = $('#form');
     form.submit(function (e) {
+        results = [];
+        result_amount = 0;
+        page = 0;
         query = form.find('input[name="query"]').val().trim();
         if (query === "") {
             return false;
         }
-        table = document.getElementById("result_table");
         keywords = query.split(" ");
         $.ajax('/query_benchmarks?query_json='
             + encodeURI(JSON.stringify(
                 {'keywords':keywords})))
             .done(function(data) {
                 results = data.results;
-                for (index = 0; index < results.length; ++index) {
-                    var result_row = table.insertRow(-1)
-                    var docker_name_cell = result_row.insertCell(0)
-                    var docker_name = results[index].docker_name
-                    docker_name_cell.innerHTML = docker_name
-                }
+                result_amount = results.length;
+                update_result_table();
                 console.log(data);
             })
         return false;
     });
 });
+
+function update_result_table() {
+    // clear result
+    var table_body = document.getElementById("result_table_body");
+    table_body.innerHTML = '';
+    var start_index = results_per_page * page;
+    var limit = Math.min(result_amount, start_index + 25);
+    for (index = start_index; index < limit; ++index) {
+        var result_row = table_body.insertRow(-1);
+        var docker_name_cell = result_row.insertCell(0);
+        var docker_name = results[index].docker_name;
+        docker_name_cell.innerHTML = docker_name;
+    }
+    if (result_amount > results_per_page) {
+        show_nav_buttons()
+        var amount_pages = (Math.floor(result_amount / results_per_page) + 1)
+        var page_info = "Page " + (page + 1) + " of " + amount_pages
+        document.getElementById("page_info").innerHTML = page_info
+    } else {
+        hide_nav_buttons()  
+        document.getElementById("page_info").innerHTML = ""      
+    }
+}
+
+function prev_page() {
+    if (page > 0) {
+        page = page - 1;
+    }
+    update_result_table();
+}
+
+function next_page() {
+    if ((page + 1) * results_per_page < result_amount) {
+        page = page + 1;
+    }
+    update_result_table();
+}
+
+function hide_nav_buttons() {
+    document.getElementById("next_button").style.display = "none";
+    document.getElementById("previous_button").style.display = "none";
+}
+
+function show_nav_buttons() {
+    document.getElementById("next_button").style.display = "inline-block";
+    document.getElementById("previous_button").style.display = "inline-block";
+}
+
+window.onload = function() {
+    hide_nav_buttons()
+    document.getElementById("page_info").style.display = "inline-block";
+}
