a13146646f6f000205f7866b7fe2c7edb6da30a9
Christophe <christophe.misc+git@protonmail.ch>
2020-11-17 18:51:43 +0100

    Reformat and refactor ajax.py
    Move benchmark json packing code to helper method

--- app/view/ajax.py
+++ app/view/ajax.py
@@ -8,14 +8,18 @@ Provided are:
 
 import json
 from abc import ABC, abstractmethod
+from typing import List
+
 from flask import request, Response
 from flask.blueprints import Blueprint
 from .type_aliases import JSON
+from ..model.data_types import Benchmark
 from ..model.facade import DatabaseFacade, facade
 
 
 class AJAXHandler(ABC):
     """Abstract class to represent any AJAX request endpoint."""
+
     @abstractmethod
     def fetch_data(self, query: JSON) -> JSON:
         """Fetch data corresponding to given query."""
@@ -45,43 +49,66 @@ class ResultSearchAJAX(SearchAJAXHandler):
         results_dict = {"results": []}
         results = self._facade.query_results(query)
         for result in results:
-            result_dict = {}
-            # decode and add to structure to avoid dealing with storing json within jsonj
-            result_dict["data"] = json.loads(result.get_json())
-            result_dict["uuid"] = result.get_uuid()
-            result_dict["site"] = result.get_site().get_short_name()
-            result_dict["benchmark"] = result.get_benchmark().get_docker_name()
-            result_dict["uploader"] = result.get_uploader().get_email()
-            result_dict["tags"] = [tag.get_name() for tag in result.get_tags()]
+            result_dict = {
+                "data": json.loads(result.get_json()), "uuid": result.get_uuid(),
+                "site": result.get_site().get_short_name(),
+                "benchmark": result.get_benchmark().get_docker_name(),
+                "uploader": result.get_uploader().get_email(),
+                "tags": [tag.get_name() for tag in result.get_tags()]
+            }
+            # decode and add to structure to avoid dealing with storing json within json
             results_dict["results"].append(result_dict)
 
         return json.dumps(results_dict)
+
+
+def _pack_benchmarks(benchmarks: List[Benchmark]):
+    results_dict = {"results": []}
+    for benchmark in benchmarks:
+        result_dict = {}
+        # do not display hidden benchmarks (= new ones)
+        if benchmark.get_hidden():
+            continue
+        # decode and add to structure to avoid dealing with storing json within json
+        result_dict["docker_name"] = benchmark.get_docker_name()
+        result_dict["uploader"] = benchmark.get_uploader().get_email()
+        results_dict["results"].append(result_dict)
+
+    return json.dumps(results_dict)
 
 
 class BenchmarkSearchAJAX(SearchAJAXHandler):
     """AJAX handler for benchmark searches with keywords."""
 
     def __init__(self, facade_: DatabaseFacade):
-        """Set up a new BenchmarkSearchAJAX using the DatabaseFacade."""
+        """Set up a new BenchmarkAJAXBase using the DatabaseFacade."""
         self._facade = facade_
 
     def find_results(self, query: JSON) -> JSON:
         """Fetch benchmarks corresponding to given query."""
-        results_dict = {"results": []}
+
         keywords = json.loads(query)['keywords']
         benchmarks = self._facade.query_benchmarks(keywords)
+        return _pack_benchmarks(benchmarks)
 
-        for benchmark in benchmarks:
-            result_dict = {}
-            # do not display hidden benchmarks (= new ones)
-            if benchmark.get_hidden():
-                continue
-            # decode and add to structure to avoid dealing with storing json within jsonj
-            result_dict["docker_name"] = benchmark.get_docker_name()
-            result_dict["uploader"] = benchmark.get_uploader().get_email()
-            results_dict["results"].append(result_dict)
 
-        return json.dumps(results_dict)
+class BenchmarkFetchAJAXHandler(AJAXHandler):
+    """AJAX handler for fetching benchmarks."""
+
+    def __init__(self, facade_: DatabaseFacade):
+        """Set up a new BenchmarkAJAXBase using the DatabaseFacade."""
+        self._facade = facade_
+
+    def fetch_data(self, query: JSON = None) -> JSON:
+        """Fetch all benchmarks independent of given query."""
+        return self.fetch_benchmarks()
+
+    def fetch_benchmarks(self) -> JSON:
+        """Fetch all benchmarks."""
+
+        benchmarks = self._facade.get_benchmarks()
+        return _pack_benchmarks(benchmarks)
+
 
 class SiteFetchAJAXHandler(AJAXHandler):
     """AJAX handler for fetching sites."""
@@ -110,6 +137,7 @@ class SiteFetchAJAXHandler(AJAXHandler):
             results_dict["results"].append(result_dict)
         return json.dumps(results_dict)
 
+
 class TagFetchAJAXHandler(AJAXHandler):
     """AJAX handler for fetching tags."""
 
@@ -126,39 +154,12 @@ class TagFetchAJAXHandler(AJAXHandler):
         results_dict = {"results": []}
         tags = self._facade.get_tags()
         for tag in tags:
-            result_dict = {}
-            result_dict["name"] = tag.get_name()
-            result_dict["description"] = tag.get_description()
+            result_dict = {
+                "name": tag.get_name(),
+                "description": tag.get_description()
+            }
             results_dict["results"].append(result_dict)
         return json.dumps(results_dict)
-
-class BenchmarkFetchAJAXHandler(AJAXHandler):
-    """AJAX handler for fetching benchmarks."""
-
-    def __init__(self, facade_: DatabaseFacade):
-        """Set up a new BenchmarkFetchAJAXHandler using the DatabaseFacade."""
-        self._facade = facade_
-
-    def fetch_data(self, query: JSON = None) -> JSON:
-        """Fetch all benchmarks independent of given query."""
-        return self.fetch_benchmarks()
-
-    def fetch_benchmarks(self) -> JSON:
-        """Fetch all benchmarks."""
-        results_dict = {"results": []}
-        benchmarks = self._facade.get_benchmarks()
-        for benchmark in benchmarks:
-            result_dict = {}
-            # do not display hidden benchmarks (= new ones)
-            if benchmark.get_hidden():
-                continue
-            # decode and add to structure to avoid dealing with storing json within jsonj
-            result_dict["docker_name"] = benchmark.get_docker_name()
-            result_dict["uploader"] = benchmark.get_uploader().get_email()
-            results_dict["results"].append(result_dict)
-
-        return json.dumps(results_dict)
-
 
 
 ajax_blueprint = Blueprint('ajax', __name__)
@@ -173,6 +174,7 @@ def query_results():
     handler = ResultSearchAJAX(facade)
     return Response(handler.fetch_data(query_json), mimetype='application/json')
 
+
 @ajax_blueprint.route('/query_benchmarks')
 def query_benchmarks():
     """HTTP endpoint for benchmark AJAX queries."""
@@ -182,17 +184,20 @@ def query_benchmarks():
     handler = BenchmarkSearchAJAX(facade)
     return Response(handler.fetch_data(query_json), mimetype='application/json')
 
+
 @ajax_blueprint.route('/fetch_sites')
 def fetch_sites():
     """HTTP endpoint for site AJAX fetches."""
     handler = SiteFetchAJAXHandler(facade)
     return Response(handler.fetch_data(), mimetype='application/json')
 
+
 @ajax_blueprint.route('/fetch_tags')
 def fetch_tags():
     """HTTP endpoint for tag AJAX fetches."""
     handler = TagFetchAJAXHandler(facade)
     return Response(handler.fetch_data(), mimetype='application/json')
+
 
 @ajax_blueprint.route('/fetch_benchmarks')
 def fetch_benchmarks():
