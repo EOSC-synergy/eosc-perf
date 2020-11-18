470dde08f510b4c3c0a065433bf7690826a08d2f
Christophe <christophe.misc+git@protonmail.ch>
2020-11-19 00:22:02 +0100

    Fix documentation and type hints for leftover files

--- eosc_perf/configuration.py
+++ eosc_perf/configuration.py
@@ -1,23 +1,32 @@
 """Configuration structure for config files to the application."""
 import os
+from typing import Any
+
 import yaml
+
 
 class ConfigHandler:
     """Helper for config state."""
+
+    config: dict = {}
+
     def __init__(self):
-        self.config = {}
+        """Set up a new configuration."""
         self.reset()
 
-    def _get_defaults(self):
-        """Get default configuration values."""
+    def _get_defaults(self) -> dict:
+        """Get default configuration values.
+        Returns:
+            dict: A dictionary containing default configuration values.
+        """
         defaults = {
             'debug': False,
             'debug-db-reset': True,
             'debug-db-dummy-items': True,
             'database-path': '',
             'admin_affiliations': ['example@kit.edu'],
-            'secret_key': '!secret',
             'debug_admin_affiliations': ['example2@kit.edu'],
+            'secret_key': '!secret',
             'oidc_client_secret': '',
             'oidc_redirect_hostname': 'localhost',
             'upload_license_filename': 'upload_license.txt',
@@ -26,8 +35,13 @@ class ConfigHandler:
 
         return defaults
 
-    def _load_config(self, load_file: bool = False):
-        """Load the config file from 'config.ini'."""
+    def _load_config(self, load_file: bool = False) -> dict:
+        """Load the config file from 'config.ini'.
+        Args:
+            load_file (bool): True if the configuration should be read from `config.ini`.
+        Returns:
+            dict: A dictionary containing all loaded configuration values.
+        """
         defaults = self._get_defaults()
         if load_file and os.path.exists('config.yaml'):
             with open('config.yaml') as file:
@@ -42,12 +56,21 @@ class ConfigHandler:
 
         return defaults
 
-    def set(self, tag, value):
-        """Set a value."""
+    def set(self, tag: str, value: Any):
+        """Set a value.
+        Args:
+            tag (str): The configuration value to change.
+            value (Any): The new value to set it to.
+        """
         self.config[tag] = value
 
-    def get(self, tag):
-        """Get a value."""
+    def get(self, tag: str) -> Any:
+        """Get a value.
+        Args:
+            tag (str): The configuration value to get.
+        Returns:
+            Any: The value of the requested configuration value.
+        """
         if not tag in self.config:
             raise ValueError("unknown config value requested")
         return self.config[tag]
@@ -59,5 +82,6 @@ class ConfigHandler:
     def reload(self):
         """Reset and reload values from file."""
         self.config = self._load_config(True)
+
 
 configuration: ConfigHandler = ConfigHandler()
--- eosc_perf/flask_app.py
+++ eosc_perf/flask_app.py
@@ -21,49 +21,48 @@ from .view.pages.search_result import result_search_blueprint
 from .view.pages.privacy_policy import privacy_blueprint
 from .view.pages.json_display import display_json_blueprint
 
-def create_app():
-    """Create the flask app object.
 
-    Args:
-        config (dict): A dictionary containing the configuration values."""
-    flask_app = Flask(__name__)
+def create_app():
+    """Create the flask app object."""
+    flask_application = Flask(__name__)
 
     if 'sphinx' in sys.modules:
-        return flask_app
+        return flask_application
 
     configuration.reload()
 
     if configuration.get('debug'):
-        flask_app.config['DEBUG'] = True
-        #app.config['SQLALCHEMY_ECHO'] = True
+        flask_application.config['DEBUG'] = True
+        # app.config['SQLALCHEMY_ECHO'] = True
         print("Running in debug mode")
     else:
         print("Running in production mode")
 
-    flask_app.app_context().push()
-    configure_database(flask_app)
-    configure_authenticator(flask_app)
+    flask_application.app_context().push()
+    configure_database(flask_application)
+    configure_authenticator(flask_application)
 
     if configuration.get('debug') and configuration.get('debug-db-demo-items'):
         add_demo()
     if configuration.get('debug') and configuration.get('debug-db-filler-items'):
         add_filler()
 
-    flask_app.register_blueprint(ajax_blueprint)
-    flask_app.register_blueprint(diagram_blueprint)
-    flask_app.register_blueprint(info_blueprint)
-    flask_app.register_blueprint(result_report_blueprint)
-    flask_app.register_blueprint(benchmark_review_blueprint)
-    flask_app.register_blueprint(site_review_blueprint)
-    flask_app.register_blueprint(view_report_blueprint)
-    flask_app.register_blueprint(add_benchmark_blueprint)
-    flask_app.register_blueprint(benchmark_search_blueprint)
-    flask_app.register_blueprint(authenticator_blueprint)
-    flask_app.register_blueprint(upload_json_blueprint)
-    flask_app.register_blueprint(result_search_blueprint)
-    flask_app.register_blueprint(privacy_blueprint)
-    flask_app.register_blueprint(display_json_blueprint)
+    flask_application.register_blueprint(ajax_blueprint)
+    flask_application.register_blueprint(diagram_blueprint)
+    flask_application.register_blueprint(info_blueprint)
+    flask_application.register_blueprint(result_report_blueprint)
+    flask_application.register_blueprint(benchmark_review_blueprint)
+    flask_application.register_blueprint(site_review_blueprint)
+    flask_application.register_blueprint(view_report_blueprint)
+    flask_application.register_blueprint(add_benchmark_blueprint)
+    flask_application.register_blueprint(benchmark_search_blueprint)
+    flask_application.register_blueprint(authenticator_blueprint)
+    flask_application.register_blueprint(upload_json_blueprint)
+    flask_application.register_blueprint(result_search_blueprint)
+    flask_application.register_blueprint(privacy_blueprint)
+    flask_application.register_blueprint(display_json_blueprint)
 
-    return flask_app
+    return flask_application
+
 
 flask_app: Flask = create_app()
