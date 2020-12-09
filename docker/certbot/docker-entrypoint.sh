d13e79cc44e75a9d6e6a6d06c82b414635fc3a54
Christophe <christophe.misc+git@protonmail.ch>
2020-12-09 02:41:32 +0100

    Add secret for nginx api credentials

--- docker/certbot/docker-entrypoint.sh
+++ docker/certbot/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+NGINX_CREDENTIALS=/run/secrets/nginx_api_credentials
+
+exec 5< ${NGINX_CREDENTIALS}
+read -r NGINX_API_USER <&5
+read -r NGINX_API_PASS <&5
+
 while :; do
   certbot renew
-  curl --fail --silent --user ${NGINX_API_USER}:${NGINX_API_PASS} http://nginx/nginx/reload
+  curl --fail --silent --user "${NGINX_API_USER}:${NGINX_API_PASS}" http://nginx/nginx/reload
   sleep 12h & wait ${!}
 done
--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+NGINX_CREDENTIALS=/run/secrets/nginx_api_credentials
+
+exec 5< ${NGINX_CREDENTIALS}
+read -r NGINX_API_USER <&5
+read -r NGINX_API_PASS <&5
+
 # API service that reloads nginx on request
 htpasswd -bc /tmp/.htpasswd "${NGINX_API_USER}" "${NGINX_API_PASS}" > /dev/null 2>&1
 (
