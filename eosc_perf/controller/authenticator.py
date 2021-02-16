EOSC-synergy/eosc-perf

commit c59453fe370f57f73ebf4e01d8b865b8d9216b9b

add exception for Aarc_g002_entitlement_Error

eosc_perf/controller/authenticator.py
@@ -6,7 +6,7 @@ from time import time
 from typing import Optional
 from urllib.request import urlopen
 import requests
-from aarc_g002_entitlement import Aarc_g002_entitlement
+from aarc_g002_entitlement import Aarc_g002_entitlement, Aarc_g002_entitlement_Error
 
 from flask import session
 from authlib.integrations.flask_client import OAuth
@@ -150,8 +150,11 @@ class Authenticator:
             return False
         for entitlement in entitlements:
             for required in self.admin_entitlements:
-                if self._match_entitlement(required, entitlement):
-                    return True
+                try:
+                    if self._match_entitlement(required, entitlement):
+                        return True
+                except Aarc_g002_entitlement_Error:
+                    return False
         return False
 
     def logout(self) -> bool:

commit 020f8c342a1d60fd6a45226ebff78b1c4215c7e9

Fix admin check
Bug introduced in c59453fe370f57f73ebf4e01d8b865b8d9216b9b but I missed it in the PR

eosc_perf/controller/authenticator.py
@@ -154,7 +154,7 @@ class Authenticator:
                     if self._match_entitlement(required, entitlement):
                         return True
                 except Aarc_g002_entitlement_Error:
-                    return False
+                    continue
         return False
 
     def logout(self) -> bool: