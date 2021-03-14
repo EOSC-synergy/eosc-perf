d060ba27c5f0133599130b65b858d9ead9e08e1b
Christophe <christophe.misc+git@protonmail.ch>
2021-03-14 19:08:56 +0100

    Add tests base for data types, boilerplate and site tests

---
+++ eosc_perf/tests/model/data_types/test_result.py
@@ -0,0 +1,11 @@
+import unittest
+
+from eosc_perf.tests.model.data_types.datatype_test_base import DatatypeTestBase
+
+
+class ResultTests(DatatypeTestBase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
