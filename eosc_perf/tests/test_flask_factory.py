a0cd8a130c2f1506cfbbae96e111ed5d15556492
Christophe <christophe.misc+git@protonmail.ch>
2021-03-14 19:52:39 +0100

    Add stub tests for flask_factory/create_app
    This will require copying/setting up the files separately for the tests. Refer to issue #106 for progress.

---
+++ eosc_perf/tests/test_flask_factory.py
@@ -0,0 +1,19 @@
+import unittest
+
+from flask import Flask
+
+from eosc_perf.flask_factory import create_app
+
+
+class FlaskFactoryTests(unittest.TestCase):
+    @unittest.skip("requires fully set up environment with files")
+    def test_create_app(self):
+        self.assertEqual(Flask, type(create_app()))
+
+    @unittest.skip("requires fully set up environment with files")
+    def test_create_app_custom_config(self):
+        self.assertEqual(Flask, type(create_app({'debug': True, 'database-path': ''})))
+
+
+if __name__ == '__main__':
+    unittest.main()
