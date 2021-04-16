63331e9a0e508424cd262aaf41b97c45705ab8db
BorjaEst <boressan@outlook.com>
2021-04-16 16:02:40 +0200

    Add dep marshmallow-sqlalchemy/flask

--- service_backend/eosc_perf/extensions.py
+++ service_backend/eosc_perf/extensions.py
@@ -3,6 +3,7 @@
 from flaat import Flaat
 from flask_bcrypt import Bcrypt
 from flask_caching import Cache
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_smorest import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -10,6 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 flaat = Flaat()
 bcrypt = Bcrypt()
 cache = Cache()
+ma = Marshmallow()
 migrate = Migrate()
 api = Api()
 db = SQLAlchemy()
