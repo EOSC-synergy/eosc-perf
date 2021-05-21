6e17d2fe808b1d5a14a3a6ee38c2ccbc17e4ee99
Christophe <christophe.misc+git@protonmail.ch>
2021-05-21 17:32:29 +0200

    Remove __main__.py because we ship with docker-compose/wsgi

--- service_frontend/eosc_perf/__main__.py
+++
@@ -1,9 +0,0 @@
-"""This module contains a main clause so you can run the application as a module directly.
-
-TODO: determine if this can be removed, e.g. if we only support deploying by docker-compose
-"""
-from .flask_factory import create_app
-
-if __name__ == '__main__':
-    flask_app = create_app()
-    flask_app.run()
