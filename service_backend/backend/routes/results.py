a6be08bb782e50254853d154f4daea3f91c46cb5
BorjaEst <boressan@outlook.com>
2021-08-11 16:21:52 +0200

    [api] document results

--- service_backend/backend/routes/results.py
+++ service_backend/backend/routes/results.py
@@ -15,27 +15,17 @@ blp = Blueprint(
 
 @blp.route('')
 class Root(MethodView):
+    """Class defining the main endpoint methods for results"""
 
     @blp.doc(operationId='GetResults')
-    @blp.arguments(args.ResultFilter, location='query', as_kwargs=True)
+    @blp.arguments(args.ResultFilter, location='query')
     @blp.response(200, schemas.Results)
-    def get(
-        self, tag_names=None, before=None, after=None, filters=None, 
-        page=1, per_page=100, **kwargs
-    ):
-        """Filters and list results.
+    def get(self, query_args):
+        """ (Free) Filters and list results
 
-        Method to get a list of results based on a query based matching
-        conditions:
-
-         - **docker_image**: Returns results matching the docker image.
-         - **docker_tag**: Returns results matching the docker tag.
-         - **site_name**: Returns results matching the site name.
-         - **flavor_name**: Returns results matching the flavor name.
-         - **tag_names**: Returns results matching the list of tag names.
-         - **upload_before**: Returns results uploaded before a ISO8601 date.
-         - **upload_after**: Returns results uploaded after a ISO8601 date.
-         - **filters**: A set of matching conditions (to describe bellow).
+        Use this method to get a list of results filtered according to your 
+        requirements. The response returns a pagination object with the
+        filtered results (if succeeds).
 
         This method allows to return results filtered by values inside the
         result. The filter is composed by 3 arguments separated by spaces
@@ -63,54 +53,36 @@ class Root(MethodView):
         order to be included in URL query strings. You can use the swagger GUI
         to automatically handle the codification, but the space separator must
         be included.
-
         ---
 
-        :param docker_image: Constraints the results returned to only those
-               using a benchmark with a specific docker image
-        :type docker_image: String, optional
-        :param docker_tag: Constraints the results returned to only those
-               using a benchmark with a specific docker tag
-        :type docker_tag: String, optional
-        :param site_name: Constraints the results returned to only those
-               performed on a specific site
-        :type site_name: String, optional
-        :param flavor_name: Constraints the results returned to only those
-               performed with an specific VM flavor
-        :type flavor_name: String, optional
-        :param tag_names: Constraints the results returned to only those
-               containing a specific set of tags
-        :type tag_names: List[String], optional
-        :param before: Constraints the results returned to only those
-               uploaded before an specific ISO8601 date
-        :type before: ISO8601 date, optional
-        :param after: Constraints the results returned to only those
-               uploaded after an specific ISO8601 date
-        :type after: ISO8601 date, optional
-        :param filters: Constraints the results returned to only those
-               that pass a certain list of conditions (filters)
-        :type filters: List[String], optional
-        :return: An sqlalchemy query which returns the filtered results.
-        :rtype: flask_sqlalchemy.BaseQuery
+        Note the URL-encoding is only needed when accessing the function as 
+        HTTP request. The python call handles the filters only as strings. 
+
+        :param query_args: The request query arguments as python dictionary
+        :type query_args: dict
+        :raises UnprocessableEntity: Request not processable
+        :return: Pagination object with filtered results
+        :rtype: :class:`flask_sqlalchemy.Pagination`
         """
-        # First query definition using main filters
-        query = models.Result.query.filter_by(**kwargs)
+        query = models.Result.query  # Create the base query
+        per_page = query_args.pop('per_page')
+        page = query_args.pop('page')
 
         # Extend query with tags
-        if type(tag_names) == list:
-            for tag_name in tag_names:
-                query = query.filter(
-                    models.Result.tag_names.in_([tag_name]))
+        for tag_name in query_args.pop('tag_names'):
+            query = query.filter(models.Result.tag_names.in_([tag_name]))
 
         # Extend query with date filter
+        before = query_args.pop('before')
         if before:
             query = query.filter(models.Result.created_at < before)
+        after = query_args.pop('after')
         if after:
             query = query.filter(models.Result.created_at > after)
 
         # Extend query with filters
         parsed_filters = []
-        for filter in filters:
+        for filter in query_args.pop('filters'):
             try:
                 path, operator, value = tuple(filter.split(' '))
             except ValueError as err:
@@ -142,7 +114,9 @@ class Root(MethodView):
         except Exception as err:
             abort(422, message={'filter': err.args})
 
+        # Model filter with remaining standard parameters
         query = query.filter(~models.Result.has_open_reports)
+        query = query.filter_by(**query_args)
         return query.paginate(page, per_page)
 
     @auth.login_required()
@@ -150,8 +124,23 @@ class Root(MethodView):
     @blp.arguments(args.ResultContext, location='query')
     @blp.arguments(schemas.Json)
     @blp.response(201, schemas.Result)
-    def post(self, args, json):
-        """Creates a new result."""
+    def post(self, query_args, body_args):
+        """(Users) Creates a new result
+
+        Use this method to create a new result in the database so it can
+        be accessed by the application users. The method returns the complete
+        created result (if succeeds).
+        ---
+
+        :param query_args: The request query arguments as python dictionary
+        :type query_args: dic
+        :param body_args: The request body arguments as python dictionary
+        :type body_args: dict
+        :raises Forbidden: The user is not registered
+        :raises NotFound: One or more query items do not exist in the database
+        :return: The result created into the database.
+        :rtype: :class:`models.Result`
+        """
         access_token = tokentools.get_access_token_from_request(request)
         user = models.User.get(token=access_token)
         report = models.Report(
@@ -159,50 +148,95 @@ class Root(MethodView):
             verdict=True  # Does not require accept from admin
         )
         return models.Result.create(
-            json=json, created_by=user, reports=[report],
-            benchmark=models.Benchmark.get(args['benchmark_id']),
-            site=models.Site.get(args['site_id']),
-            flavor=models.Flavor.get(args['flavor_id']),
-            tags=[models.Tag.get(id) for id in args['tags_ids']]
+            json=body_args, created_by=user, reports=[report],
+            benchmark=models.Benchmark.get(query_args['benchmark_id']),
+            site=models.Site.get(query_args['site_id']),
+            flavor=models.Flavor.get(query_args['flavor_id']),
+            tags=[models.Tag.get(id) for id in query_args['tags_ids']]
         )
 
 
 @blp.route('/search')
 class Search(MethodView):
+    """Class defining the search endpoint for results"""
 
     @blp.doc(operationId='SearchResults')
-    @blp.arguments(args.Search, location='query', as_kwargs=True)
+    @blp.arguments(args.Search, location='query')
     @blp.response(200, schemas.Results)
-    def get(self, terms, page=1, per_page=100):
-        """Filters and list results."""
-        search = models.Result.search(terms)
+    def get(self, query_args):
+        """(Free) Filters and list results
+
+        Use this method to get a list of results based on a general search
+        of terms. For example, calling this method with terms=v1&terms=0
+        returns all results with 'v1' and '0' on the 'docker_image',
+        'docker_tag', 'site_name', 'flavor_name' fields or 'tags'. 
+        The response returns a pagination object with the filtered results
+        (if succeeds).
+        ---
+
+        :param query_args: The request query arguments as python dictionary
+        :type query_args: dict
+        :return: Pagination object with filtered results
+        :rtype: :class:`flask_sqlalchemy.Pagination`
+        """
+        per_page = query_args.pop('per_page')
+        page = query_args.pop('page')
+        search = models.Result.search(query_args['terms'])
         search = search.filter(~models.Result.has_open_reports)
         return search.paginate(page, per_page)
 
 
 @blp.route('/<uuid:result_id>')
 class Result(MethodView):
+    """Class defining the specific result endpoint"""
 
     @blp.doc(operationId='GetResult')
     @blp.response(200, schemas.Result)
     def get(self, result_id):
-        """Retrieves result details."""
+        """(Free) Retrieves result details
+
+        Use this method to retrieve a specific result from the database.
+        ---
+
+        If no result exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param result_id: The id of the result to retrieve
+        :type result_id: uuid
+        :raises NotFound: No result with id found
+        :return: The database result using the described id
+        :rtype: :class:`models.Result`
+        """
         return models.Result.get(result_id)
 
     @auth.login_required()
     @blp.doc(operationId='EditResult')
-    @blp.arguments(schemas.TagsIds, as_kwargs=True)
+    @blp.arguments(schemas.TagsIds)
     @blp.response(204)
-    def put(self, result_id, tags_ids=None):
-        """Updates an existing result tags."""
+    def put(self, body_args, result_id):
+        """(Owner) Updates an existing result tags
+
+        Use this method to update tags on a specific result from the database.
+        ---
+
+        If no result exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param body_args: The request body arguments as python dictionary
+        :type body_args: dict
+        :param result_id: The id of the result to update
+        :type result_id: uuid
+        :raises NotFound: No result with id found
+        """
         access_token = tokentools.get_access_token_from_request(request)
         result = models.Result.get(result_id)
-        def is_owner(): 
+
+        def is_owner():
             user = models.User.get(token=access_token)
             return result.created_by.email == user.email
         if auth.valid_admin() or is_owner():
-            if tags_ids is not None:  # Empty list should pass
-                tags = [models.Tag.get(id) for id in tags_ids]
+            if 'tags_ids' in body_args:  # Empty list should pass
+                tags = [models.Tag.get(id) for id in body_args['tags_ids']]
                 result.update(tags=tags)
         else:
             abort(403)
@@ -211,35 +245,74 @@ class Result(MethodView):
     @blp.doc(operationId='DelResult')
     @blp.response(204)
     def delete(self, result_id):
-        """Deletes an existing result."""
+        """(Admins) Deletes an existing result
+
+        Use this method to delete a specific result from the database.
+        ---
+
+        If no result exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param result_id: The id of the result to delete
+        :type result_id: uuid
+        :raises NotFound: No result with id found
+        """
         models.Result.get(result_id).delete()
 
 
 @blp.route('/<uuid:result_id>/uploader')
 class Uploader(MethodView):
+    """Class defining the endpoint to retrieve a result uploader"""
 
     @auth.admin_required()
     @blp.doc(operationId='GetResultUploader')
     @blp.response(200, schemas.User)
     def get(self, result_id):
-        """Retrieves result uploader."""
+        """(Admins) Retrieves result uploader
+
+        Use this method to retrieve the uploader of a specific result
+        from the database.
+        ---
+
+        If no result exists with the indicated id, then 404 NotFound
+        exception is raised.        
+
+        :param result_id: The id of the result created by the returned user
+        :type result_id: uuid
+        :raises NotFound: No result with id found
+        """
         return models.Result.get(result_id).created_by
 
 
 @blp.route('/<uuid:result_id>/report')
 class Report(MethodView):
+    """Class defining the endpoint to create result reports"""
 
     @auth.login_required()
     @blp.doc(operationId='AddResultReport')
     @blp.arguments(schemas.ReportCreate)
     @blp.response(201, schemas.Report)
-    def post(self, json, result_id):
-        """Creates a result report."""
+    def post(self, body_args, result_id):
+        """(Users) Creates a result report
+
+        Use this method to create a report for a specific result so the
+        administrators are aware of issues. The reported result is hidden
+        from generic responses until the issue is corrected and approved
+        by the administrators. 
+        ---
+
+        If no result exists with the indicated id, then 404 NotFound
+        exception is raised.        
+
+        :param result_id: The id of the result created by the returned user
+        :type result_id: uuid
+        :raises NotFound: No result with id found
+        """
         access_token = tokentools.get_access_token_from_request(request)
         result = models.Result.get(result_id)
         report = models.Report(
             created_by=models.User.get(token=access_token),
-            message=json['message']
+            message=body_args['message']
         )
         result.update(reports=result.reports+[report])
         return report
