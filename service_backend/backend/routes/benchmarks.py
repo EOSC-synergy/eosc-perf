553c85cb41868926f2403425bc38b9f6719c4bc4
BorjaEst <boressan@outlook.com>
2021-08-11 14:02:54 +0200

    [api] document benchmark routes

--- service_backend/backend/routes/benchmarks.py
+++ service_backend/backend/routes/benchmarks.py
@@ -17,9 +17,9 @@ class Root(MethodView):
     """Class defining the main endpoint methods for benchmarks"""
 
     @blp.doc(operationId='GetBenchmarks')
-    @blp.arguments(args.BenchmarkFilter, location='query', as_kwargs=True)
+    @blp.arguments(args.BenchmarkFilter, location='query')
     @blp.response(200, schemas.Benchmarks)
-    def get(self, page=1, per_page=100, **kwargs):
+    def get(self, query_args):
         """(Free) Filters and list benchmarks
 
         Use this method to get a list of benchmarks filtered according to your 
@@ -27,71 +27,129 @@ class Root(MethodView):
         filtered benchmarks (if succeeds).
         ---
 
-        :param page: The page number to retrieve, defaults to 1
-        :type page: int, optional
-        :param per_page: N of items to be displayed per page, defaults to 100
-        :type per_page: int, optional
+        :param query_args: The request query arguments as python dictionary
+        :type query_args: dict
         :return: Pagination object with filtered benchmarks
         :rtype: :class:`flask_sqlalchemy.Pagination`
         """
-        query = models.Benchmark.query.filter_by(**kwargs)
+        per_page = query_args.pop('per_page')
+        page = query_args.pop('page')
+        query = models.Benchmark.query.filter_by(**query_args)
         query = query.filter(~models.Benchmark.has_open_reports)
         return query.paginate(page, per_page)
 
     @auth.login_required()
     @blp.doc(operationId='AddBenchmark')
-    @blp.arguments(schemas.BenchmarkCreate, as_kwargs=True)
+    @blp.arguments(schemas.BenchmarkCreate)
     @blp.response(201, schemas.Benchmark)
-    def post(self, **kwargs):
+    def post(self, body_args):
         """(Users) Creates a new benchmark
-    
+
         Use this method to create a new benchmarks in the database so it can
         be accessed by the application users. The method returns the complete
         created benchmark (if succeeds).
         ---
 
+        :param body_args: The request body arguments as python dictionary
+        :type body_args: dict
         :return: The benchmark created into the database.
         :rtype: :class:`models.Benchmark`
-        """        
+        """
         access_token = tokentools.get_access_token_from_request(request)
         user = models.User.get(token=access_token)
         report = models.Report(created_by=user, message="New benchmark")
-        return models.Benchmark.create(created_by=user, reports=[report], **kwargs)
+        return models.Benchmark.create(
+            created_by=user, reports=[report], **body_args
+        )
 
 
 @blp.route('/search')
 class Search(MethodView):
+    """Class defining the search endpoint for benchmarks"""
 
     @blp.doc(operationId='SearchBenchmarks')
-    @blp.arguments(args.Search, location='query', as_kwargs=True)
+    @blp.arguments(args.Search, location='query')
     @blp.response(200, schemas.Benchmarks)
-    def get(self, terms, page=1, per_page=100):
-        """Filters and list benchmarks."""
-        search = models.Benchmark.search(terms)
+    def get(self, query_args):
+        """(Free) Filters and list benchmarks
+
+        Use this method to get a list of benchmarks based on a general search
+        of terms. For example, calling this method with terms=v1&terms=0
+        returns all benchmarks with 'v1' and '0' on the 'docker_image',
+        'docker_tag' or 'description' fields. The response returns a
+        pagination object with the filtered benchmarks (if succeeds).
+        ---
+
+        :param query_args: The request query arguments as python dictionary
+        :type query_args: dict
+        :return: Pagination object with filtered benchmarks
+        :rtype: :class:`flask_sqlalchemy.Pagination`
+        """
+        per_page = query_args.pop('per_page')
+        page = query_args.pop('page')
+        search = models.Benchmark.search(query_args['terms'])
         search = search.filter(~models.Benchmark.has_open_reports)
         return search.paginate(page, per_page)
 
 
 @blp.route('/<uuid:benchmark_id>')
 class Benchmark(MethodView):
+    """Class defining the specific benchmark endpoint"""
 
     @blp.doc(operationId='GetBenchmark')
     @blp.response(200, schemas.Benchmark)
     def get(self, benchmark_id):
-        """Retrieves benchmark details."""
+        """(Free) Retrieves benchmark details
+
+        Use this method to retrieve a specific benchmark from the database.
+        ---
+
+        If no benchmark exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param benchmark_id: The id of the benchmark to retrieve
+        :type benchmark_id: uuid
+        :raises werkzeug.exceptions.NotFound: No benchmark with id found
+        :return: The database benchmark using the described id
+        :rtype: :class:`models.Benchmark`
+        """
         return models.Benchmark.get(benchmark_id)
 
     @auth.admin_required()
     @blp.doc(operationId='EditBenchmark')
-    @blp.arguments(schemas.BenchmarkEdit, as_kwargs=True)
+    @blp.arguments(schemas.BenchmarkEdit)
     @blp.response(204)
-    def put(self, benchmark_id, **kwargs):
-        """Updates an existing benchmark."""
-        models.Benchmark.get(benchmark_id).update(**kwargs)
+    def put(self, body_args, benchmark_id):
+        """(Admins) Updates an existing benchmark
+
+        Use this method to update a specific benchmark from the database.
+        ---
+
+        If no benchmark exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param body_args: The request body arguments as python dictionary
+        :type body_args: dict
+        :param benchmark_id: The id of the benchmark to update
+        :type benchmark_id: uuid
+        :raises werkzeug.exceptions.NotFound: No benchmark with id found
+        """
+        models.Benchmark.get(benchmark_id).update(**body_args)
 
     @auth.admin_required()
     @blp.doc(operationId='DelBenchmark')
     @blp.response(204)
     def delete(self, benchmark_id):
-        """Deletes an existing benchmark."""
+        """(Admins) Deletes an existing benchmark
+
+        Use this method to delete a specific benchmark from the database.
+        ---
+
+        If no benchmark exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param benchmark_id: The id of the benchmark to delete
+        :type benchmark_id: uuid
+        :raises werkzeug.exceptions.NotFound: No benchmark with id found
+        """
         models.Benchmark.get(benchmark_id).delete()
