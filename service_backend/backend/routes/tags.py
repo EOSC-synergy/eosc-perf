67876030f34f2d4feafcfac30e8974b5980f8971
Borja Esteban <boressan@outlook.com>
2021-08-12 15:16:48 +0200

    [api] document tags routes

--- service_backend/backend/routes/tags.py
+++ service_backend/backend/routes/tags.py
@@ -2,6 +2,8 @@
 from backend.extensions import auth
 from backend.models import models
 from backend.schemas import args, schemas
+from flaat import tokentools
+from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
@@ -12,55 +14,139 @@ blp = Blueprint(
 
 @blp.route('')
 class Root(MethodView):
+    """Class defining the main endpoint methods for tags"""
 
     @blp.doc(operationId='GetTags')
-    @blp.arguments(args.TagFilter, location='query', as_kwargs=True)
+    @blp.arguments(args.TagFilter, location='query')
     @blp.response(200, schemas.Tags)
-    def get(self, page=1, per_page=100, **kwargs):
-        """Filters and list tags."""
-        query = models.Tag.query.filter_by(**kwargs)
+    def get(self, query_args):
+        """(Free) Filters and list tags
+
+        Use this method to get a list of tags filtered according to your 
+        requirements. The response returns a pagination object with the
+        filtered tags (if succeeds).
+        ---
+
+        :param query_args: The request query arguments as python dictionary
+        :type query_args: dict
+        :return: Pagination object with filtered tags
+        :rtype: :class:`flask_sqlalchemy.Pagination`
+        """
+        per_page = query_args.pop('per_page')
+        page = query_args.pop('page')
+        query = models.Tag.query.filter_by(**query_args)
         return query.paginate(page, per_page)
 
     @auth.login_required()
     @blp.doc(operationId='AddTag')
-    @blp.arguments(schemas.TagCreate, as_kwargs=True)
+    @blp.arguments(schemas.TagCreate)
     @blp.response(201, schemas.Tag)
-    def post(self, **kwargs):
-        """Creates a new tag."""
-        return models.Tag.create(**kwargs)
+    def post(self, body_args):
+        """(Users) Creates a new tag
+
+        Use this method to create a new tags in the database so it can
+        be accessed by the application users. The method returns the complete
+        created tag (if succeeds).
+        ---
+
+        :param body_args: The request body arguments as python dictionary
+        :type body_args: dict
+        :raises Forbidden: The user is not registered
+        :raises UnprocessableEntity: Request not processable
+        :raises Conflict: Created object conflicts a database item
+        :return: The tag created into the database.
+        :rtype: :class:`models.Tag`
+        """
+        access_token = tokentools.get_access_token_from_request(request)
+        models.User.get(token=access_token)  # Check user is registered
+        return models.Tag.create(**body_args)
 
 
 @blp.route('/search')
 class Search(MethodView):
+    """Class defining the search endpoint for tags"""
 
-    @blp.arguments(args.Search, location='query', as_kwargs=True)
+    @blp.arguments(args.Search, location='query')
     @blp.response(200, schemas.Tags)
-    def get(self, terms, page=1, per_page=100):
-        """Filters and list tags."""
-        search = models.Tag.search(terms)
+    def get(self, query_args):
+        """(Free) Filters and list tags
+
+        Use this method to get a list of tags based on a general search
+        of terms. For example, calling this method with terms=v1&terms=0
+        returns all tags with 'v1' and '0' on the 'docker_image',
+        'docker_tag' or 'description' fields. The response returns a
+        pagination object with the filtered tags (if succeeds).
+        ---
+
+        :param query_args: The request query arguments as python dictionary
+        :type query_args: dict
+        :return: Pagination object with filtered tags
+        :rtype: :class:`flask_sqlalchemy.Pagination`
+        """
+        per_page = query_args.pop('per_page')
+        page = query_args.pop('page')
+        search = models.Tag.search(query_args['terms'])
         return search.paginate(page, per_page)
 
 
 @blp.route('/<uuid:tag_id>')
 class Tag(MethodView):
+    """Class defining the specific tag endpoint"""
 
     @blp.doc(operationId='GetTag')
     @blp.response(200, schemas.Tag)
     def get(self, tag_id):
-        """Retrieves tag details."""
+        """(Free) Retrieves tag details
+
+        Use this method to retrieve a specific tag from the database.
+        ---
+
+        If no tag exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param tag_id: The id of the tag to retrieve
+        :type tag_id: uuid
+        :raises NotFound: No tag with id found
+        :return: The database tag using the described id
+        :rtype: :class:`models.Tag`
+        """
         return models.Tag.get(tag_id)
 
     @auth.admin_required()
     @blp.doc(operationId='EditTag')
-    @blp.arguments(schemas.TagEdit, as_kwargs=True)
+    @blp.arguments(schemas.TagEdit)
     @blp.response(204)
-    def put(self, tag_id, **kwargs):
-        """Updates an existing tag."""
-        models.Tag.get(tag_id).update(**kwargs)
+    def put(self, body_args, tag_id):
+        """(Admins) Updates an existing tag
+
+        Use this method to update a specific tag from the database.
+        ---
+
+        If no tag exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param body_args: The request body arguments as python dictionary
+        :type body_args: dict
+        :param tag_id: The id of the tag to update
+        :type tag_id: uuid
+        :raises NotFound: No tag with id found
+        """
+        models.Tag.get(tag_id).update(**body_args)
 
     @auth.admin_required()
     @blp.doc(operationId='DelTag')
     @blp.response(204)
     def delete(self, tag_id):
-        """Deletes an existing tag."""
+        """(Admins) Deletes an existing tag
+
+        Use this method to delete a specific tag from the database.
+        ---
+
+        If no tag exists with the indicated id, then 404 NotFound
+        exception is raised.
+
+        :param tag_id: The id of the tag to delete
+        :type tag_id: uuid
+        :raises NotFound: No tag with id found
+        """
         models.Tag.get(tag_id).delete()
