03fc0b54e0b384309e13d24ed50f66ab7590259f
BorjaEst <boressan@outlook.com>
2021-08-23 12:36:12 +0200

    [api] doc extensions

--- service_backend/backend/extensions.py
+++ service_backend/backend/extensions.py
@@ -1,5 +1,13 @@
-"""Extensions module.
-Each extension is initialized in the app factory located in app.py
+"""Extensions module. This module implements the loading of multiple
+flask extensions to extend the basic provided functionality to the API
+requirements.
+
+For more information about flask and extensions see:
+https://flask.palletsprojects.com/en/2.0.x/extensions
+
+Each extension requires of a specific class initialization which is 
+lately initialized in the application factory using the settings and
+configurations from the environment. 
 """
 from flask_bcrypt import Bcrypt
 from flask_caching import Cache
@@ -9,9 +17,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 from .authorization import Authorization
 
-bcrypt = Bcrypt()
-cache = Cache()
-migrate = Migrate()
-api = Api()
-db = SQLAlchemy()
+
+#: Flask extension that provides support for handling oidc Access Tokens
 auth = Authorization()
+
+#: Flask extension that provides bcrypt hashing utilities
+bcrypt = Bcrypt()
+
+#: Flask extension that adds caching support for various backends
+cache = Cache()
+
+#: Flask framework library for creating REST APIs (i.e. OpenAPI)
+api = Api()
+
+#: Flask extension hat adds support for SQLAlchemy
+db = SQLAlchemy()
+
+#: Flask extension that handles SQLAlchemy database migrations using Alembic
+migrate = Migrate()
--- service_backend/docs/source/_backend/backend.rst
+++ service_backend/docs/source/_backend/backend.rst
@@ -23,6 +23,7 @@ Extensions module
 
 .. automodule:: backend.extensions
    :members:
+   :member-order: bysource
    :undoc-members:
    :show-inheritance:
 
