2de38ebcec8f68bbafee7a69c7367b39e61e3526
Christophe <christophe.misc+git@protonmail.ch>
2021-08-31 18:32:05 +0200

    [app] Add popover search menu for benchmark, site, flavor

---
+++ frontend-js/src/components/SearchPopover/index.tsx
@@ -0,0 +1,238 @@
+import React, { ReactNode, useState } from 'react';
+import { useQuery } from 'react-query';
+import { SearchForm } from './searchForm';
+import { Table } from './table';
+import { LoadingOverlay } from '../loadingOverlay';
+import { Benchmark, Flavor, Site } from '../../api';
+import { getHelper } from '../../api-helpers';
+import { Paginated, Paginator } from '../pagination';
+import { Button, Col, OverlayTrigger, Popover, Row } from 'react-bootstrap';
+import { Identifiable } from '../identifiable';
+
+function SimpleSearchPopover<Item extends Identifiable>(props: {
+    queryKeyPrefix: string;
+    tableName: string;
+    endpoint: string;
+    item?: Item;
+    setItem: (item: Item) => void;
+    display: (item?: Item) => ReactNode;
+    displayRow: (item: Item) => ReactNode;
+}) {
+    //const [resultsPerPage, setResultsPerPage] = useState(10);
+    const [page, setPage] = useState(0);
+
+    const [searchString, setSearchString] = useState<string>('');
+
+    let items = useQuery(
+        props.queryKeyPrefix + '-page-' + page + '-' + searchString,
+        () => {
+            return getHelper<Paginated<Item>>(props.endpoint, undefined, {
+                // split here so we can add searchString to key to fetch automatically
+                terms: searchString.split(' '),
+            });
+        },
+        {
+            refetchOnWindowFocus: false, // do not spam queries
+        }
+    );
+
+    const popover = (
+        <Popover id="benchmarkSelect" style={{ maxWidth: '576px', width: 'auto' }}>
+            <Popover.Title as="h3">Benchmark Search</Popover.Title>
+            <Popover.Content>
+                <SearchForm setSearchString={setSearchString} />
+                <div style={{ position: 'relative' }}>
+                    {items.isLoading && (
+                        <>
+                            <Table<Item>
+                                setItem={() => {}}
+                                items={[]}
+                                tableName={props.tableName}
+                                displayItem={() => {
+                                    return <></>;
+                                }}
+                            />
+                            <LoadingOverlay />
+                        </>
+                    )}
+                    {items.isError && <div>Failed to fetch benchmarks!</div>}
+                    {items.isSuccess && (
+                        <Table<Item>
+                            items={items.data.data.items!}
+                            setItem={props.setItem}
+                            tableName={props.tableName}
+                            displayItem={props.displayRow}
+                        />
+                    )}
+                </div>
+                <Row>
+                    {items.isSuccess && (
+                        <Col>
+                            <Paginator pagination={items.data.data} navigateTo={setPage} />
+                        </Col>
+                    )}
+                    <Col md="auto">
+                        <Button className="m-1">+ New</Button>
+                    </Col>
+                </Row>
+            </Popover.Content>
+        </Popover>
+    );
+
+    return (
+        <div>
+            {props.display(props.item)}{' '}
+            <OverlayTrigger trigger="click" placement="bottom" overlay={popover} rootClose>
+                <Button variant="success">Select</Button>
+            </OverlayTrigger>
+        </div>
+    );
+}
+
+export function BenchmarkSearchPopover(props: {
+    benchmark?: Benchmark;
+    setBenchmark: (benchmark: Benchmark) => void;
+}) {
+    function display(benchmark?: Benchmark) {
+        return (
+            <>
+                Benchmark:{' '}
+                {benchmark ? (
+                    <a href={'https://hub.docker.com/repository/docker/' + benchmark.docker_image}>
+                        {benchmark.docker_image + ':' + benchmark.docker_tag}
+                    </a>
+                ) : (
+                    <div className="text-muted" style={{ display: 'inline-block' }}>
+                        None
+                    </div>
+                )}
+            </>
+        );
+    }
+
+    function displayRow(benchmark: Benchmark) {
+        return (
+            <>
+                <a
+                    title={benchmark.docker_image + ':' + benchmark.docker_tag}
+                    href="#"
+                    onClick={() => props.setBenchmark(benchmark)}
+                >
+                    {benchmark.docker_image + ':' + benchmark.docker_tag}
+                </a>
+                <div>
+                    {benchmark.description}
+                    <br />
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <SimpleSearchPopover<Benchmark>
+            queryKeyPrefix="benchmark"
+            tableName="Benchmark"
+            endpoint="/benchmarks/search"
+            item={props.benchmark}
+            setItem={props.setBenchmark}
+            display={display}
+            displayRow={displayRow}
+        />
+    );
+}
+
+export function SiteSearchPopover(props: { site?: Site; setSite: (site: Site) => void }) {
+    function display(site?: Site) {
+        return (
+            <>
+                Site:{' '}
+                {site ? (
+                    <>{site.name}</>
+                ) : (
+                    <div className="text-muted" style={{ display: 'inline-block' }}>
+                        None
+                    </div>
+                )}
+            </>
+        );
+    }
+
+    function displayRow(site: Site) {
+        return (
+            <>
+                <a title={site.name} href="#" onClick={() => props.setSite(site)}>
+                    {site.name}
+                </a>
+                <div>
+                    {site.description}
+                    <br />
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <SimpleSearchPopover<Site>
+            queryKeyPrefix="site"
+            tableName="Site"
+            endpoint="/sites/search"
+            item={props.site}
+            setItem={props.setSite}
+            display={display}
+            displayRow={displayRow}
+        />
+    );
+}
+
+export function FlavorSearchPopover(props: {
+    site?: Site;
+    flavor?: Flavor;
+    setFlavor: (flavor: Flavor) => void;
+}) {
+    function display(flavor?: Flavor) {
+        return (
+            <>
+                Flavor:{' '}
+                {flavor ? (
+                    <>{flavor.name}</>
+                ) : (
+                    <div className="text-muted" style={{ display: 'inline-block' }}>
+                        None
+                    </div>
+                )}
+            </>
+        );
+    }
+
+    function displayRow(flavor: Flavor) {
+        return (
+            <>
+                <a title={flavor.name} href="#" onClick={() => props.setFlavor(flavor)}>
+                    {flavor.name}
+                </a>
+                <div>
+                    {flavor.description}
+                    <br />
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <>
+            {props.site ? (
+                <SimpleSearchPopover<Flavor>
+                    queryKeyPrefix="flavor"
+                    tableName="Flavor"
+                    endpoint={'/sites/' + props.site.id + '/flavors'}
+                    item={props.flavor}
+                    setItem={props.setFlavor}
+                    display={display}
+                    displayRow={displayRow}
+                />
+            ) : (
+                <></>
+            )}
+        </>
+    );
+}
---
+++ frontend-js/src/components/SearchPopover/searchForm.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+
+export function SearchForm(props: { setSearchString: (params: string) => void }) {
+    const [message, setMessage] = useState('');
+
+    return (
+        <Form>
+            <Form.Group as={Row}>
+                <Form.Label className="sr-only" column>
+                    Query
+                </Form.Label>
+                <Col>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter your query here, keywords separated by spaces"
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                </Col>
+                <Col md="auto">
+                    <Button variant="info" onClick={() => props.setSearchString(message)}>
+                        Search
+                    </Button>
+                </Col>
+            </Form.Group>
+        </Form>
+    );
+}
