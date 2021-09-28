8dec81a362f294499dc12f3f5241c33b4a184eb6
BorjaEst <boressan@outlook.com>
2021-09-28 09:09:50 +0200

    [api] Clean benchmark tests names

--- service_backend/tests/benchmarks/test_routes.py
+++ service_backend/tests/benchmarks/test_routes.py
@@ -20,7 +20,7 @@ class TestList:
         {'sort_by': "+upload_datetime"},
         {'sort_by': "+id"}
     ])
-    def test_GET_200(self, response_GET, url):
+    def test_200(self, response_GET, url):
         """GET method succeeded 200."""
         assert response_GET.status_code == 200
         asserts.match_pagination(response_GET.json, url)
@@ -35,7 +35,7 @@ class TestList:
         {'bad_key': "This is a non expected query key"},
         {'sort_by': "Bad sort command"}
     ])
-    def test_GET_422(self, response_GET):
+    def test_422(self, response_GET):
         """GET method fails 422 if bad request body."""
         assert response_GET.status_code == 422
 
@@ -51,7 +51,7 @@ class TestCreate:
         {'docker_image': "b2", 'docker_tag': "v2.0", 'json_schema': {'x': 1},
          'description': "This is a long benchmark description"},
     ])
-    def test_POST_201(self, response_POST, url, body):
+    def test_201(self, response_POST, url, body):
         """POST method succeeded 201."""
         assert response_POST.status_code == 201
         asserts.match_query(response_POST.json, url)
@@ -64,7 +64,7 @@ class TestCreate:
         {'docker_image': "b1", 'docker_tag': "v2.0", 'json_schema': {'x': 1}},
         {}  # Empty body
     ])
-    def test_POST_401(self, response_POST):
+    def test_401(self, response_POST):
         """POST method fails 401 if not authorized."""
         assert response_POST.status_code == 401
 
@@ -74,7 +74,7 @@ class TestCreate:
     @mark.parametrize('body', indirect=True, argvalues=[
         {'docker_image': "b1", 'docker_tag': "v2.0", 'json_schema': {'x': 1}}
     ])
-    def test_POST_403(self, response_POST):
+    def test_403(self, response_POST):
         """POST method fails 403 if user not registered."""
         assert response_POST.status_code == 403
 
@@ -86,7 +86,7 @@ class TestCreate:
         {'docker_image': "b2", 'docker_tag': "v1.0", 'json_schema': {'x': 1}}
     ])
     @mark.filterwarnings("ignore:.*conflicts.*:sqlalchemy.exc.SAWarning")
-    def test_POST_409(self, response_POST):
+    def test_409(self, response_POST):
         """POST method fails 409 if resource already exists."""
         assert response_POST.status_code == 409
 
@@ -103,7 +103,7 @@ class TestCreate:
         {'json_schema': {'x': 1}},
         {}  # Empty body
     ])
-    def test_POST_422(self, response_POST):
+    def test_422(self, response_POST):
         """POST method fails 422 if missing required."""
         assert response_POST.status_code == 422
 
@@ -122,7 +122,7 @@ class TestSearch:
         {'sort_by': "+upload_datetime"},
         {'sort_by': "+id"}
     ])
-    def test_GET_200(self, response_GET, url):
+    def test_200(self, response_GET, url):
         """GET method succeeded 200."""
         assert response_GET.status_code == 200
         asserts.match_pagination(response_GET.json, url)
@@ -137,7 +137,7 @@ class TestSearch:
         {'bad_key': "This is a non expected query key"},
         {'sort_by': "Bad sort command"}
     ])
-    def test_GET_422(self, response_GET):
+    def test_422(self, response_GET):
         """GET method fails 422 if bad request body."""
         assert response_GET.status_code == 422
 
@@ -150,13 +150,13 @@ class TestSearch:
 ])
 class TestGet:
 
-    def test_GET_200(self, benchmark, response_GET):
+    def test_200(self, benchmark, response_GET):
         """GET method succeeded 200."""
         assert response_GET.status_code == 200
         asserts.match_benchmark(response_GET.json, benchmark)
 
     @mark.parametrize('request_id', [uuid4()], indirect=True)
-    def test_GET_404(self, response_GET):
+    def test_404(self, response_GET):
         """GET method fails 404 if no id found."""
         assert response_GET.status_code == 404
 
@@ -174,7 +174,7 @@ class TestUpdate:
         {'docker_image': "new_name", 'docker_tag': "v1.0",
          'json_schema': {"x": 2}},
     ])
-    def test_PUT_204(self, body, response_PUT, benchmark):
+    def test_204(self, body, response_PUT, benchmark):
         """PUT method succeeded 204."""
         assert response_PUT.status_code == 204
         json = schemas.Benchmark().dump(benchmark)
@@ -183,7 +183,7 @@ class TestUpdate:
     @mark.parametrize('body', indirect=True,  argvalues=[
         {'docker_tag': "new_tag"}
     ])
-    def test_PUT_401(self, benchmark, response_PUT):
+    def test_401(self, benchmark, response_PUT):
         """PUT method fails 401 if not authorized."""
         assert response_PUT.status_code == 401
         assert benchmark == models.Benchmark.query.get(benchmark.id)
@@ -194,7 +194,7 @@ class TestUpdate:
         {'docker_image': "new_name", 'docker_tag': "v1.0",
          'json_schema': {"x": 2}},
     ])
-    def test_PUT_404(self, benchmark, response_PUT):
+    def test_404(self, benchmark, response_PUT):
         """PUT method fails 404 if no id found."""
         assert response_PUT.status_code == 404
         assert benchmark == models.Benchmark.query.get(benchmark.id)
@@ -203,7 +203,7 @@ class TestUpdate:
     @mark.parametrize('body', indirect=True,  argvalues=[
         {'bad_field': ""}
     ])
-    def test_PUT_422(self, benchmark, response_PUT):
+    def test_422(self, benchmark, response_PUT):
         """PUT method fails 422 if bad request body."""
         assert response_PUT.status_code == 422
         assert benchmark == models.Benchmark.query.get(benchmark.id)
@@ -218,25 +218,25 @@ class TestUpdate:
 class TestDelete:
 
     @mark.usefixtures('grant_admin')
-    def test_DELETE_204(self, benchmark, response_DELETE):
+    def test_204(self, benchmark, response_DELETE):
         """DELETE method succeeded 204."""
         assert response_DELETE.status_code == 204
         assert models.Benchmark.query.get(benchmark.id) is None
 
-    def test_DELETE_401(self, benchmark, response_DELETE):
+    def test_401(self, benchmark, response_DELETE):
         """DELETE method fails 401 if not authorized."""
         assert response_DELETE.status_code == 401
         assert models.Benchmark.query.get(benchmark.id) is not None
 
     @mark.usefixtures('grant_logged')
-    def test_DELETE_403(self, benchmark, response_PUT):
+    def test_403(self, benchmark, response_PUT):
         """DELETE method fails 403 if method forbidden."""
         assert response_PUT.status_code == 403
         assert models.Benchmark.query.get(benchmark.id) is not None
 
     @mark.usefixtures('grant_admin')
     @mark.parametrize('request_id', [uuid4()], indirect=True)
-    def test_DELETE_404(self, benchmark, response_DELETE):
+    def test_404(self, benchmark, response_DELETE):
         """DELETE method fails 404 if no id found."""
         assert response_DELETE.status_code == 404
         assert models.Benchmark.query.get(benchmark.id) is not None
