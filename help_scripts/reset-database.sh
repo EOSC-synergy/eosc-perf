5524eba27de62f7b3dedb502043e0f62d00914a9
BorjaEst <boressan@outlook.com>
2021-09-28 16:10:26 +0200

    [api] Add context and comments to reset-database.sh

--- help_scripts/reset-database.sh
+++ help_scripts/reset-database.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+# This script will reset the database on docker-compose
+# Not it does not create a migrations so service_backend/migrations
+# folder should be up to date.
+
 docker-compose down
 docker volume rm eosc-perf_postgres
 docker-compose up -d database
