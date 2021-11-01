96a1fc10deef948082222de5565d772ac1f1d247
Christophe <christophe.misc+git@protonmail.ch>
2021-11-01 18:05:06 +0100

    [app] Remove empty test file

--- frontend-js/src/appRouter.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-/*test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});*/
