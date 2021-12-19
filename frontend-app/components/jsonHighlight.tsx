6dd53477515a21bae889cab79003769779305294
Christophe <christophe.misc+git@protonmail.ch>
2021-12-19 12:34:17 +0100

    [app] Document JsonHighlight

--- frontend-app/components/jsonHighlight.tsx
+++ frontend-app/components/jsonHighlight.tsx
@@ -3,6 +3,12 @@ import React, { ReactElement, useEffect } from 'react';
 import prism from 'prismjs';
 import 'prismjs/components/prism-json';
 
+/**
+ * Display JSON text with syntax highlighting
+ * @param {{children: React.ReactNode}} props
+ * @returns {React.ReactElement}
+ * @constructor
+ */
 export function JsonHighlight(props: { children: React.ReactNode }): ReactElement {
     useEffect(() => {
         prism.highlightAll();
