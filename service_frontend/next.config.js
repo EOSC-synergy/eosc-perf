13ef890725f4a7457ac3889098f8265d373ad640
Christophe <christophe.misc+git@protonmail.ch>
2021-12-20 03:33:38 +0100

    [app] Reinstate frontend as service for production using nextjs

--- service_frontend/next.config.js
+++ service_frontend/next.config.js
@@ -1,6 +1,13 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true'
-});
+let withBundleAnalyzer = undefined;
+try {
+    withBundleAnalyzer = require('@next/bundle-analyzer')({
+        enabled: process.env.ANALYZE === 'true'
+    });
+} catch (e) {
+    console.log('No @next/bundle-analyzer, assuming production');
+    withBundleAnalyzer = () => {
+    };
+}
 
 /** @type {import('next').NextConfig} */
 module.exports = {
