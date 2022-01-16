28c1fbd896869b229d78291cfdaa441be08abce3
Christophe <christophe.misc+git@protonmail.ch>
2022-01-16 03:48:38 +0100

    [app] Change TOS Check component to show modal instead of redirecting to separate page
    Closes #142

--- service_frontend/components/termsOfServiceCheck.tsx
+++ service_frontend/components/termsOfServiceCheck.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
-import Link from 'next/link';
+import React, { useState } from 'react';
+import { Badge, Button, Form, Modal } from 'react-bootstrap';
+import { TermsOfService } from './termsOfService';
+import actionable from '../styles/actionable.module.css';
 
 /**
  * Form element component to make sure user has accepted terms of service
@@ -12,17 +13,48 @@ export function TermsOfServiceCheck(props: {
     accepted: boolean;
     setAccepted: (accepted: boolean) => void;
 }) {
+    const [showTOS, setShowTOS] = useState(false);
+
     return (
-        <Form.Check
-            type="switch"
-            label={
-                <>
-                    I have read and accept the{' '}
-                    <Link href="/terms-of-service">Terms of Service</Link>
-                </>
-            }
-            checked={props.accepted}
-            onChange={() => props.setAccepted(!props.accepted)}
-        />
+        <>
+            <Form.Group>
+                <Form.Check
+                    type="switch"
+                    label={
+                        <>
+                            I have read and accept the{' '}
+                            <Badge
+                                bg="secondary"
+                                onClick={() => setShowTOS(true)}
+                                className={actionable.actionable}
+                            >
+                                Terms of Service
+                            </Badge>
+                        </>
+                    }
+                    checked={props.accepted}
+                    onChange={() => props.setAccepted(!props.accepted)}
+                />
+            </Form.Group>
+            <Modal show={showTOS} onHide={() => setShowTOS(false)} size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Acceptable Use Policy</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <TermsOfService />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="success"
+                        onClick={() => {
+                            props.setAccepted(true);
+                            setShowTOS(false);
+                        }}
+                    >
+                        Accept
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
