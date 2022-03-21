6fe01d5fa2f5897b12523e2a9be0675e2baa30b1
BorjaEst <boressan@outlook.com>
2022-03-21 15:23:11 +0100

    replace infos_to_user by infos_to_object

--- service_backend/backend/authorization.py
+++ service_backend/backend/authorization.py
@@ -86,7 +86,7 @@ class Authorization(Flaat):
                 abort(401, messages={'error': error_msg})
             return user
 
-        return super().inject_object(infos_to_user=_get_user, key="user")
+        return super().inject_object(infos_to_object=_get_user, key="user")
 
     def inject_is_admin(self):
         """ inject boolean kwarg "is_admin" into a view function
@@ -95,4 +95,4 @@ class Authorization(Flaat):
         def _is_admin(user_infos: UserInfos):
             return self.get_admin_requirement().is_satisfied_by(user_infos)
 
-        return super().inject_object(infos_to_user=_is_admin, key="is_admin", strict=True)
+        return super().inject_object(infos_to_object=_is_admin, key="is_admin", strict=True)
