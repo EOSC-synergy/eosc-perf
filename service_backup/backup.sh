9c50525561627fb98540b3b0c537b66d6f3e09bb
Borja Esteban <boressan@outlook.com>
2022-08-30 17:27:55 +0200

    Add support for database password secret

--- service_backup/backup.sh
+++ service_backup/backup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-export PGHOST=${DB_HOST}
-export PGPORT=${DB_PORT}
-export PGUSER=${DB_USER}
-export PGPASSWORD=${DB_PASSWORD}
-pg_dump -d "${DB_DATABASE}" -F t -f /workdir/${DB_DATABASE}."$(date +'%Y-%m-%d_%H-%M-%S')".sql.tar
+export PGHOST=${POSTGRES_HOST}
+export PGPORT=${POSTGRES_PORT}
+export PGUSER=${POSTGRES_USER}
+export PGPASSWORD=$(cat ${POSTGRES_PASSWORD_FILE})
+
+pg_dump -d "${POSTGRES_DB}" -F t -f /workdir/${POSTGRES_DB}."$(date +'%Y-%m-%d_%H-%M-%S')".sql.tar
